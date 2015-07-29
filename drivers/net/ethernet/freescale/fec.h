eeed52a3c9ae716c3d8aa72e92d1600426602dab
Stoica Cosmin-Stefan <cosminstefan.stoica@freescale.com>
2015-07-29 15:03:09 +0300

    fec: Add initial support for S32V234
    Based on commit a6e7c1a218f5
    ("ALB-6 Add initial support for ENET on S32V234")
    
    Issue: ALB-2545
    Signed-off-by: Stoica Cosmin-Stefan <cosminstefan.stoica@freescale.com>
    Signed-off-by: Dan Nica <dan.nica@nxp.com>

--- drivers/net/ethernet/freescale/fec.h
+++ drivers/net/ethernet/freescale/fec.h
@@ -7,6 +7,7 @@
  *
  *	(C) Copyright 2000-2005, Greg Ungerer (gerg@snapgear.com)
  *	(C) Copyright 2000-2001, Lineo (www.lineo.com)
+ *	(C) Copyright 2015 Freescale Semiconductor, Inc.
  */
 
 /****************************************************************************/
@@ -20,8 +21,8 @@
 #include <linux/timecounter.h>
 
 #if defined(CONFIG_M523x) || defined(CONFIG_M527x) || defined(CONFIG_M528x) || \
-    defined(CONFIG_M520x) || defined(CONFIG_M532x) || defined(CONFIG_ARM) || \
-    defined(CONFIG_ARM64) || defined(CONFIG_COMPILE_TEST)
+	defined(CONFIG_M520x) || defined(CONFIG_M532x) || defined(CONFIG_ARM) || \
+	defined(CONFIG_ARM64) || defined(CONFIG_COMPILE_TEST) || defined(CONFIG_ARCH_S32)
 /*
  *	Just figures, Motorola would have to change the offsets for
  *	registers in the same peripheral device on different models
@@ -196,7 +197,7 @@
  *	Evidently, ARM SoCs have the FEC block generated in a
  *	little endian mode so adjust endianness accordingly.
  */
-#if defined(CONFIG_ARM) || defined(CONFIG_ARM64)
+#if defined(CONFIG_ARM) || defined(CONFIG_ARM64) || defined(CONFIG_ARCH_S32)
 #define fec32_to_cpu le32_to_cpu
 #define fec16_to_cpu le16_to_cpu
 #define cpu_to_fec32 cpu_to_le32
@@ -293,8 +294,8 @@ struct bufdesc_ex {
 #define BD_ENET_TX_IINS		0x08000000
 
 
-/* This device has up to three irqs on some platforms */
-#define FEC_IRQ_NUM		3
+/* This device has up to four irqs on some platforms */
+#define FEC_IRQ_NUM		4
 
 /* Maximum number of queues supported
  * ENET with AVB IP can support up to 3 independent tx queues and rx queues.
