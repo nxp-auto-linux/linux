e71d23c0e30de5d616a2a797a5872fb47e0759d2
Irina Presa <irina.presa@nxp.com>
2016-09-22 14:32:18 +0300

    crypto: cse3: Move CSE3 driver code in its own directory
    Move CSE3 driver code in its owm directory.
    
    Signed-off-by: Irina Presa <irina.presa@nxp.com>

--- drivers/crypto/cse/cse3_capi.h
+++ drivers/crypto/cse/cse3_capi.h
@@ -1,13 +1,13 @@
 /* SPDX-License-Identifier: GPL-2.0+ */
 /*
  * Freescale Cryptographic Services Engine (CSE3) Device Driver
- * CSE3 Linux HWRng Interface
+ * CSE3 Linux Crypto API Interface
  *
  * Copyright (c) 2016 Freescale Semiconductor, Inc.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 or
- * later as publishhed by the Free Software Foundation.
+ * later as published by the Free Software Foundation.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -15,16 +15,27 @@
  * GNU General Public License for more details.
  */
 
-#ifndef _CSE_RNG_H
-#define _CSE_RNG_H
+#ifndef _CSE_CAPI_H
+#define _CSE_CAPI_H
 
-#ifdef CONFIG_CRYPTO_DEV_FSL_CSE3_HWRNG
-void cse_register_rng(void);
+#ifdef CONFIG_CRYPTO_DEV_FSL_CSE3_CRYPTO_API
+
+/**
+ * Register and unregister supported CSE3 crypto API algorithms
+ */
+void cse_register_crypto_api(void);
+void cse_unregister_crypto_api(void);
 
 #else
-static inline void cse_register_rng(void)
+
+static inline void cse_register_crypto_api(void)
 {
 }
+
+static inline void cse_unregister_crypto_api(void)
+{
+}
+
 #endif
 
-#endif /* _CSE_RNG_H */
+#endif /* _CSE_CAPI_H */
