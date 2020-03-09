b2057240bd9a516dfcb36468a7529906600489f7
Jose Abreu <Jose.Abreu@synopsys.com>
2020-03-09 09:36:25 +0100

    net: phylink: Test if MAC/PCS support Autoneg
    We may have cases where MAC or PCS do not support Autoneg. Check if it
    is supported after validate callback is called.
    
    Issue: ALB-7210
    Signed-off-by: Jose Abreu <Jose.Abreu@synopsys.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/phy/phylink.c
+++ drivers/net/phy/phylink.c
@@ -327,6 +327,9 @@ static int phylink_parse_mode(struct phylink *pl, struct fwnode_handle *fwnode)
 				    "failed to validate link configuration for in-band status\n");
 			return -EINVAL;
 		}
+
+		/* Check if MAC/PCS also supports Autoneg. */
+		pl->link_config.an_enabled = phylink_test(pl->supported, Autoneg);
 	}
 
 	return 0;
