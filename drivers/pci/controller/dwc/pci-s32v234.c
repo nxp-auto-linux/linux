5d50d318c08232714c426128701135062f4200a5
Jakub Sosnovec <jakub.sosnovec@nxp.com>
2020-09-22 11:02:59 +0200

    pcie: dma: Set send_signal_to_user and fix memory corruption for siginfo
    This is a combination of the following commits:
        Set send_signal_to_user after DMA finishes [6dd2c01d8e02]
        Fix memory corruption for siginfo [5903b976707f]
    
    Issue: ALB-5982
    Signed-off-by: Jakub Sosnovec <jakub.sosnovec@nxp.com>
    Signed-off-by: Bogdan-Gabriel Roman <bogdan-gabriel.roman@nxp.com>

--- drivers/pci/controller/dwc/pci-s32v234.c
+++ drivers/pci/controller/dwc/pci-s32v234.c
@@ -1317,6 +1317,8 @@ static int s32v234_pcie_probe(struct platform_device *pdev)
 	pcie->dev = dev;
 	pcie->ops = &s32v234_pcie_ops;
 
+	s32v234_pp->send_signal_to_user = send_signal_to_user;
+
 	/* Added for PCI abort handling */
 	dbi_base = platform_get_resource(pdev, IORESOURCE_MEM, 0);
 	pcie->dbi_base = devm_ioremap_resource(dev, dbi_base);
@@ -1389,7 +1391,7 @@ static int s32v234_pcie_probe(struct platform_device *pdev)
 		}
 		dw_pcie_dma_clear_regs(pcie, &s32v234_pp->dma);
 
-		memset(&s32v234_pp->info, 0, sizeof(struct siginfo));
+		memset(&s32v234_pp->info, 0, sizeof(struct kernel_siginfo));
 		s32v234_pp->info.si_signo = SIGUSR1;
 		s32v234_pp->info.si_code = SI_USER;
 		s32v234_pp->info.si_int = 0;
