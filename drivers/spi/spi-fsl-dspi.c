67fda991b70ab9f5496afe37913aacd0d3e9d95b
Ciprian Marian Costea <ciprianmarian.costea@nxp.com>
2021-01-06 16:42:13 +0200

    spi: spi-fsl-dspi: Add support for S32 platforms
    1. Add compatible for S32 platforms
    Based on (commit id: '718360c7c9bf')
    and (commit id: 'c37ee2129761')
    
    2. Update memory map definitions for s32v234
    Based on commit 4a6fe448c2aa ("spi: spi-fsl-dspi: Update memory map
    definitions for s32v234") and commit 4f4d4e46e8e3 ("ALB-8 S32V234 DSPI
    has 5 TX and 5 RX FIFOS").
    
    3. Kconfig: Add S32 targets
    
    Issue: ALB-2130
    Issue: ALB-4486
    Issue: ALB-5969
    Signed-off-by: Stoica Cosmin-Stefan <cosmin.stoica@nxp.com>
    Signed-off-by: Dan Nica <dan.nica@nxp.com>
    Signed-off-by: Larisa Grigore <Larisa.Grigore@nxp.com>
    Signed-off-by: Stefan-Gabriel Mirea <stefan-gabriel.mirea@nxp.com>
    Signed-off-by: Ciprian Marian Costea <ciprianmarian.costea@nxp.com>

--- drivers/spi/spi-fsl-dspi.c
+++ drivers/spi/spi-fsl-dspi.c
@@ -133,6 +133,7 @@ enum {
 	LX2160A,
 	MCF5441X,
 	VF610,
+	S32CC,
 };
 
 static const struct fsl_dspi_devtype_data devtype_data[] = {
@@ -189,6 +190,11 @@ static const struct fsl_dspi_devtype_data devtype_data[] = {
 		.trans_mode		= DSPI_DMA_MODE,
 		.max_clock_factor	= 8,
 		.fifo_size		= 16,
+	},
+	[S32CC] = {
+		.trans_mode		= DSPI_XSPI_MODE,
+		.max_clock_factor	= 1,
+		.fifo_size		= 5,
 	},
 };
 
@@ -1073,6 +1079,9 @@ static const struct of_device_id fsl_dspi_dt_ids[] = {
 	}, {
 		.compatible = "fsl,lx2160a-dspi",
 		.data = &devtype_data[LX2160A],
+	}, {
+		.compatible = "nxp,s32cc-dspi",
+		.data = &devtype_data[S32CC],
 	},
 	{ /* sentinel */ }
 };
