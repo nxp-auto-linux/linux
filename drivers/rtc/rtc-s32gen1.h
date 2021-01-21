b1074eb5910181ae5824b25483d1b2beb951414e
Ghennadi Procopciuc <Ghennadi.Procopciuc@nxp.com>
2021-01-21 15:33:15 +0200

    rtc-s32gen1: Implement read_time callback
    Issue: ALB-5823
    Signed-off-by: Ghennadi Procopciuc <Ghennadi.Procopciuc@nxp.com>

--- drivers/rtc/rtc-s32gen1.h
+++ drivers/rtc/rtc-s32gen1.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 /*
- * Copyright 2019 NXP
+ * Copyright 2019,2021 NXP
  *
  * RTC register definition for the S32GEN1
  *
@@ -36,6 +36,7 @@
 #define RTCC_DIV32EN		BIT(10)
 /* RTCS fields */
 #define RTCS_RTCF		BIT(29)
+#define RTCS_INV_RTC		BIT(18)
 #define RTCS_APIF		BIT(13)
 #define RTCS_ROVRF		BIT(10)
 
