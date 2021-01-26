ed614a58f89e58f14de483c26f47450045c88109
Larisa Grigore <Larisa.Grigore@nxp.com>
2021-01-26 16:41:10 +0200

    tty: serial: linflexuart: Fix fsl_linflexuart LINFBRR calculation
    Baudrate is computed as LIN_CLK / (OSR × LDIV) when ROSE is 1 or LIN_CLK
    / (16 × LDIV) when ROSE is 0. LDIV is an unsigned fixed point number.
    The mantissa is coded into 20 bits of LINIBRR and the fraction is coded on
    4 bits of LINFBRR so setting the baudrate means setting LINFBRR and
    LINIBRR.
    LINIBRR is the integer part of LIN_CLK / f(ROSE) * baudrate and LINFBRR
    is the fractional part of it multiplied by 16, where f(ROSE) can be 16
    or OSR value.
    LINFBRR wasn't correctly computed. If we consider divisr = LIN_CLK and
    dividr = f(ROSE) * baudrate, LINFBRR should be the fractional part of
    (divisr/dividr) multiplied by 16 and not (divisr%dividr)*16.
    
    Issue: ALB-594
    Signed-off-by: Larisa Grigore <Larisa.Grigore@nxp.com>

--- drivers/tty/serial/fsl_linflexuart.c
+++ drivers/tty/serial/fsl_linflexuart.c
@@ -866,6 +866,19 @@ static void linflex_shutdown(struct uart_port *port)
 	}
 }
 
+static int
+linflex_ldiv_multiplier(struct linflex_port *sport)
+{
+	unsigned int mul = LINFLEX_LDIV_MULTIPLIER;
+	unsigned long cr;
+
+	cr = readl(sport->port.membase + UARTCR);
+	if (cr & LINFLEXD_UARTCR_ROSE)
+		mul = LINFLEXD_UARTCR_OSR(cr);
+
+	return mul;
+}
+
 static void
 linflex_set_termios(struct uart_port *port, struct ktermios *termios,
 		    struct ktermios *old)
@@ -984,10 +997,10 @@ linflex_set_termios(struct uart_port *port, struct ktermios *termios,
 	       sport->port.membase + UARTCR);
 
 	divisr = sport->port.uartclk;	//freq in Hz
-	dividr = (baud * 16);
+	dividr = (baud * linflex_ldiv_multiplier(sport));
 
-	ibr = divisr/dividr;
-	fbr = (divisr%dividr)*16;
+	ibr = divisr / dividr;
+	fbr = ((divisr % dividr) * 16 / dividr) & 0xF;
 
 	writel(ibr, sport->port.membase + LINIBRR);
 	writel(fbr, sport->port.membase + LINFBRR);
@@ -1335,7 +1348,7 @@ linflex_console_get_options(struct linflex_port *sport, int *baud, int *parity,
 
 	uartclk = clk_get_rate(sport->clk);
 
-	baud_raw = uartclk / (16 * (sbr + brfa / 32));
+	baud_raw = uartclk / (linflex_ldiv_multiplier(sport) * ibr);
 
 	if (*baud != baud_raw)
 		pr_info("Serial: Console linflex rounded baud rate from %d to %d\n",
