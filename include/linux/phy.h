3e35f1d17d6c1710d7f7e36599a8d1d61ed031a1
Wong Vee Khee <vee.khee.wong@linux.intel.com>
2021-03-24 00:46:40 +0800

    net: phy: add genphy_c45_loopback
    Add generic code to enable C45 PHY loopback into the common phy-c45.c
    file. This will allow C45 PHY drivers aceess this by setting
    .set_loopback.
    
    Suggested-by: Heiner Kallweit <hkallweit1@gmail.com>
    Signed-off-by: Wong Vee Khee <vee.khee.wong@linux.intel.com>
    Reviewed-by: Heiner Kallweit <hkallweit1@gmail.com>
    Reviewed-by: Andrew Lunn <andrew@lunn.ch>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 0ef25ed104ac17fa0586fbb076f24a5e8940b966)
    Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

--- include/linux/phy.h
+++ include/linux/phy.h
@@ -104,6 +104,7 @@ typedef enum {
 	/* 10GBASE-KR, XFI, SFI - single lane 10G Serdes */
 	PHY_INTERFACE_MODE_10GKR,
 	PHY_INTERFACE_MODE_USXGMII,
+
 	PHY_INTERFACE_MODE_MAX,
 } phy_interface_t;
 
@@ -181,6 +182,7 @@ static inline const char *phy_modes(phy_interface_t interface)
 		return "10gbase-kr";
 	case PHY_INTERFACE_MODE_USXGMII:
 		return "usxgmii";
+
 	default:
 		return "unknown";
 	}
