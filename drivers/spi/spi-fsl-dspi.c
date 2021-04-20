5fb25b83542be7591715c2be38dfef6ee14911eb
Larisa Grigore <larisa.grigore@nxp.com>
2021-04-20 15:15:14 +0300

    spi-fsl-dspi: Reinitialize DSPI regs after resume for s32gen1
    After resume, DSPI registers (MCR and SR) need to be
    reinitialized in s32gen1 platform case.
    
    Issue: ALB-6826
    Issue: ALB-6347
    Signed-off-by: Larisa Grigore <larisa.grigore@nxp.com>

--- drivers/spi/spi-fsl-dspi.c
+++ drivers/spi/spi-fsl-dspi.c
@@ -1142,6 +1142,42 @@ static inline int is_s32gen1_dspi(struct fsl_dspi *data)
 	return data->devtype_data == &devtype_data[S32GEN1];
 }
 
+static int dspi_init(struct fsl_dspi *dspi)
+{
+	unsigned int mcr;
+
+	/* Set idle states for all chip select signals to high */
+	if (is_s32v234_dspi(dspi))
+		mcr = SPI_MCR_PCSIS(0xFF);
+	else
+		mcr = SPI_MCR_PCSIS(GENMASK(dspi->ctlr->max_native_cs - 1, 0));
+
+	if (dspi->devtype_data->trans_mode == DSPI_XSPI_MODE)
+		mcr |= SPI_MCR_XSPI;
+	if (!spi_controller_is_slave(dspi->ctlr))
+		mcr |= SPI_MCR_MASTER;
+
+	regmap_write(dspi->regmap, SPI_MCR, mcr);
+	regmap_write(dspi->regmap, SPI_SR, SPI_SR_CLEAR);
+
+	switch (dspi->devtype_data->trans_mode) {
+	case DSPI_XSPI_MODE:
+		regmap_write(dspi->regmap, SPI_RSER, SPI_RSER_CMDTCFE);
+		break;
+	case DSPI_DMA_MODE:
+		regmap_write(dspi->regmap, SPI_RSER,
+			     SPI_RSER_TFFFE | SPI_RSER_TFFFD |
+			     SPI_RSER_RFDFE | SPI_RSER_RFDFD);
+		break;
+	default:
+		dev_err(&dspi->pdev->dev, "unsupported trans_mode %u\n",
+			dspi->devtype_data->trans_mode);
+		return -EINVAL;
+	}
+
+	return 0;
+}
+
 #ifdef CONFIG_PM_SLEEP
 static int dspi_suspend(struct device *dev)
 {
@@ -1168,6 +1204,16 @@ static int dspi_resume(struct device *dev)
 	if (ret)
 		return ret;
 	spi_controller_resume(dspi->ctlr);
+
+	if (is_s32gen1_dspi(dspi) ||
+			dspi->devtype_data == &devtype_data[S32_SLAVE]) {
+		ret = dspi_init(dspi);
+		if (ret) {
+			dev_err(dev, "failed to initialize dspi\n");
+			return ret;
+		}
+	}
+
 	if (dspi->irq)
 		enable_irq(dspi->irq);
 
@@ -1302,42 +1348,6 @@ static void dspi_set_pinctrl(struct platform_device *pdev,
 			dev_err(&pdev->dev,
 				"failed to switch to slave pinctrl: %d\n", ret);
 	}
-}
-
-static int dspi_init(struct fsl_dspi *dspi)
-{
-	unsigned int mcr;
-
-	/* Set idle states for all chip select signals to high */
-	if (is_s32v234_dspi(dspi))
-		mcr = SPI_MCR_PCSIS(0xFF);
-	else
-		mcr = SPI_MCR_PCSIS(GENMASK(dspi->ctlr->max_native_cs - 1, 0));
-
-	if (dspi->devtype_data->trans_mode == DSPI_XSPI_MODE)
-		mcr |= SPI_MCR_XSPI;
-	if (!spi_controller_is_slave(dspi->ctlr))
-		mcr |= SPI_MCR_MASTER;
-
-	regmap_write(dspi->regmap, SPI_MCR, mcr);
-	regmap_write(dspi->regmap, SPI_SR, SPI_SR_CLEAR);
-
-	switch (dspi->devtype_data->trans_mode) {
-	case DSPI_XSPI_MODE:
-		regmap_write(dspi->regmap, SPI_RSER, SPI_RSER_CMDTCFE);
-		break;
-	case DSPI_DMA_MODE:
-		regmap_write(dspi->regmap, SPI_RSER,
-			     SPI_RSER_TFFFE | SPI_RSER_TFFFD |
-			     SPI_RSER_RFDFE | SPI_RSER_RFDFD);
-		break;
-	default:
-		dev_err(&dspi->pdev->dev, "unsupported trans_mode %u\n",
-			dspi->devtype_data->trans_mode);
-		return -EINVAL;
-	}
-
-	return 0;
 }
 
 static int dspi_slave_abort(struct spi_master *master)
