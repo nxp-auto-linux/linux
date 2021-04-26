e53ab9f4632a74adc76e4f97104279ca8e41781d
Radu Pirea (NXP OSS) <radu-nicolae.pirea@oss.nxp.com>
2021-04-26 10:00:03 +0300

    ptp: ptp_clock: make scaled_ppm_to_ppb static inline
    Make scaled_ppm_to_ppb static inline to be able to build drivers that
    use this function even with PTP_1588_CLOCK disabled.
    
    Issue: ALB-6773
    
    Signed-off-by: Radu Pirea (NXP OSS) <radu-nicolae.pirea@oss.nxp.com>

--- include/linux/ptp_clock_kernel.h
+++ include/linux/ptp_clock_kernel.h
@@ -3,6 +3,7 @@
  * PTP 1588 clock support
  *
  * Copyright (C) 2010 OMICRON electronics GmbH
+ * Copyright (C) 2021 NXP
  */
 
 #ifndef _PTP_CLOCK_KERNEL_H_
@@ -173,6 +174,32 @@ struct ptp_clock_event {
 	};
 };
 
+/**
+ * scaled_ppm_to_ppb() - convert scaled ppm to ppb
+ *
+ * @ppm:    Parts per million, but with a 16 bit binary fractional field
+ */
+static inline s32 scaled_ppm_to_ppb(long ppm)
+{
+	/*
+	 * The 'freq' field in the 'struct timex' is in parts per
+	 * million, but with a 16 bit binary fractional field.
+	 *
+	 * We want to calculate
+	 *
+	 *    ppb = scaled_ppm * 1000 / 2^16
+	 *
+	 * which simplifies to
+	 *
+	 *    ppb = scaled_ppm * 125 / 2^13
+	 */
+	s64 ppb = 1 + ppm;
+
+	ppb *= 125;
+	ppb >>= 13;
+	return (s32)ppb;
+}
+
 #if IS_REACHABLE(CONFIG_PTP_1588_CLOCK)
 
 /**
@@ -215,14 +242,6 @@ extern void ptp_clock_event(struct ptp_clock *ptp,
  */
 
 extern int ptp_clock_index(struct ptp_clock *ptp);
-
-/**
- * scaled_ppm_to_ppb() - convert scaled ppm to ppb
- *
- * @ppm:    Parts per million, but with a 16 bit binary fractional field
- */
-
-extern long scaled_ppm_to_ppb(long ppm);
 
 /**
  * ptp_find_pin() - obtain the pin index of a given auxiliary function
