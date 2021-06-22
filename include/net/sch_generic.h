e53209401b604d8b1d204e10d052866e4e3faaea
Yunsheng Lin <linyunsheng@huawei.com>
2021-06-22 14:49:55 +0800

    net: sched: avoid unnecessary seqcount operation for lockless qdisc
    qdisc->running seqcount operation is mainly used to do heuristic
    locking on q->busylock for locked qdisc, see qdisc_is_running()
    and __dev_xmit_skb().
    
    So avoid doing seqcount operation for qdisc with TCQ_F_NOLOCK
    flag.
    
    Issue: ALB-7463
    
    Acked-by: Jakub Kicinski <kuba@kernel.org>
    Tested-by: Vladimir Oltean <vladimir.oltean@nxp.com> # flexcan
    Signed-off-by: Yunsheng Lin <linyunsheng@huawei.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/sch_generic.h
+++ include/net/sch_generic.h
@@ -185,6 +185,7 @@ static inline bool qdisc_run_begin(struct Qdisc *qdisc)
 
 nolock_empty:
 		WRITE_ONCE(qdisc->empty, false);
+		return true;
 	} else if (qdisc_is_running(qdisc)) {
 		return false;
 	}
@@ -212,11 +213,6 @@ nolock_empty:
 
 static inline void qdisc_run_end(struct Qdisc *qdisc)
 {
-#ifdef CONFIG_PREEMPT_RT
-	write_sequnlock(&qdisc->running);
-#else
-	write_seqcount_end(&qdisc->running);
-#endif
 	if (qdisc->flags & TCQ_F_NOLOCK) {
 		spin_unlock(&qdisc->seqlock);
 
@@ -231,6 +227,12 @@ static inline void qdisc_run_end(struct Qdisc *qdisc)
 			clear_bit(__QDISC_STATE_MISSED, &qdisc->state);
 			__netif_schedule(qdisc);
 		}
+	} else {
+#ifdef CONFIG_PREEMPT_RT
+		write_sequnlock(&qdisc->running);
+#else
+		write_seqcount_end(&qdisc->running);
+#endif
 	}
 }
 
