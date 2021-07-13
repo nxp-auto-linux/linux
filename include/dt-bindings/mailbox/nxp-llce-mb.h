a763eb0d3f9515b5b3e68f10b4cb3440c1586c8a
Ghennadi Procopciuc <ghennadi.procopciuc@nxp.com>
2021-07-13 14:19:11 +0300

    dt-bindings: Add LLCE logger channel
    Issue: ALB-5830, ALB-7344
    Signed-off-by: Ghennadi Procopciuc <ghennadi.procopciuc@nxp.com>

--- include/dt-bindings/mailbox/nxp-llce-mb.h
+++ include/dt-bindings/mailbox/nxp-llce-mb.h
@@ -8,9 +8,10 @@
 /*
  * LLCE Mailbox Type
  */
-#define S32G_LLCE_HIF_CONF_MB	0x0
-#define S32G_LLCE_CAN_CONF_MB	0x1
+#define S32G_LLCE_HIF_CONF_MB		0x0
+#define S32G_LLCE_CAN_CONF_MB		0x1
 #define S32G_LLCE_CAN_RX_MB		0x2
 #define S32G_LLCE_CAN_TX_MB		0x3
+#define S32G_LLCE_CAN_LOGGER_MB		0x4
 
 #endif
