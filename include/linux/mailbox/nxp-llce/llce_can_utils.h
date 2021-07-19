afa4b8807f99141e059aa44fb35ef68a87ae094e
Ghennadi Procopciuc <ghennadi.procopciuc@nxp.com>
2021-07-19 10:02:09 +0300

    llce: logger: include id and flags in dump
    CAN ID and frame flags are included in dump file.
    
    Issue: ALB-5830, ALB-7344
    Signed-off-by: Ghennadi Procopciuc <ghennadi.procopciuc@nxp.com>

--- include/linux/mailbox/nxp-llce/llce_can_utils.h
+++ include/linux/mailbox/nxp-llce/llce_can_utils.h
@@ -6,6 +6,8 @@
 #define LLCE_CAN_UTILS_H
 
 #include <linux/ctype.h>
+#include <linux/mailbox/nxp-llce/llce_can.h>
+#include <uapi/linux/can.h>
 
 static inline void unpack_word0(u32 word0, bool *rtr, bool *ide,
 				u32 *std_id, u32 *ext_id)
@@ -23,13 +25,40 @@ static inline void unpack_word0(u32 word0, bool *rtr, bool *ide,
 	*rtr = !!(word0 & LLCE_CAN_MB_RTR);
 }
 
-static inline void unpack_word1(u32 word1, bool *fdf, u8 *len, bool *brs,
+static inline u32 pack_word0(bool rtr, bool ide, u32 id)
+{
+	u32 word0;
+
+	word0 = (rtr << LLCE_CAN_MB_RTR_SHIFT);
+	word0 |= (ide << LLCE_CAN_MB_IDE_SHIFT);
+
+	if (ide)
+		word0 |= (id & CAN_EFF_MASK);
+	else
+		word0 |= (id & CAN_SFF_MASK) <<  LLCE_CAN_MB_IDSTD_SHIFT;
+
+	return word0;
+}
+
+static inline void unpack_word1(u32 word1, bool *fdf, u8 *dlc, bool *brs,
 				bool *esi)
 {
-	*len = word1 & LLCE_CAN_MB_DLC_MASK;
+	*dlc = word1 & LLCE_CAN_MB_DLC_MASK;
 	*brs = !!(word1 & LLCE_CAN_MB_BRS);
 	*esi = !!(word1 & LLCE_CAN_MB_ESI);
 	*fdf = !!(word1 & LLCE_CAN_MB_FDF);
+}
+
+static inline u32 pack_word1(bool fdf, u8 dlc, bool brs, bool esi)
+{
+	u32 word1;
+
+	word1 = (brs << LLCE_CAN_MB_BRS_SHIFT);
+	word1 |= (fdf << LLCE_CAN_MB_FDF_SHIFT);
+	word1 |= (esi << LLCE_CAN_MB_ESI_SHIFT);
+	word1 |= dlc;
+
+	return word1;
 }
 
 #endif
