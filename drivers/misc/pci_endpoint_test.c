2e026c9cb3fa7fe9c23acbd095ef2fd430b956e1
Ionut Vicovan <Ionut.Vicovan@nxp.com>
2021-07-24 03:27:44 +0300

    s32g:pci: misc/pci_endpoint_test: Workaround for vendor/device not being set properly
    The vendor/device values set from the EP test function do not get
    the same to the RC side.
    
    We add to pci_endpoint_test the values that actually get from the
    EP test function to the pcie_endpoint_test module (RC side) when
    attempting to set vendor to Frescale/NXP and device to S32G/S32V.
    
    Issue: ALB-7552
    
    Signed-off-by: Ionut Vicovan <Ionut.Vicovan@nxp.com>

--- drivers/misc/pci_endpoint_test.c
+++ drivers/misc/pci_endpoint_test.c
@@ -808,6 +808,10 @@ static const struct pci_device_id pci_endpoint_test_tbl[] = {
 	{ PCI_DEVICE(PCI_VENDOR_ID_TI, PCI_DEVICE_ID_TI_DRA72x) },
 	{ PCI_DEVICE(PCI_VENDOR_ID_FREESCALE, 0x81c0) },
 	{ PCI_DEVICE(PCI_VENDOR_ID_FREESCALE, PCI_DEVICE_ID_LX2160A) },
+	/* somehow the vendor gets 0x0 and the device last digit 0x2 */
+	{ PCI_DEVICE(0x0, PCI_DEVICE_ID_S32G),
+	  .driver_data = (kernel_ulong_t)&s32_data
+	},
 	{ PCI_DEVICE(PCI_VENDOR_ID_FREESCALE, PCI_DEVICE_ID_S32G),
 	  .driver_data = (kernel_ulong_t)&s32_data
 	},
