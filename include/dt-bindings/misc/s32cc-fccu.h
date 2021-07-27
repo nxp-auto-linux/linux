b5662de1e1fa824a781d7de00db90bd04d5355a8
Catalin Udma <catalin-dan.udma@nxp.com>
2021-07-27 08:18:04 +0300

    dt-bindings: include/dt-bindings/misc/ add s32cc-fccu
    Issue: ALB-7459
    Issue: ALB-9104
    Signed-off-by: Catalin Udma <catalin-dan.udma@nxp.com>

---
+++ include/dt-bindings/misc/s32cc-fccu.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: GPL-2.0-only OR BSD-2-Clause */
+/*
+ * Copyright 2021-2022 NXP
+ */
+#ifndef __DT_BINDINGS_S32CC_FCCU_H
+#define __DT_BINDINGS_S32CC_FCCU_H
+
+#define S32CC_FCCU_REACTION_NONE		0
+#define S32CC_FCCU_REACTION_ALARM		(1 << 1)
+#define S32CC_FCCU_REACTION_FUNC_RESET		(1 << 2)
+#define S32CC_FCCU_REACTION_NMI			(1 << 3)
+#define S32CC_FCCU_REACTION_EOUT		(1 << 4)
+
+#endif
