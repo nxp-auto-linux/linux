dbffc677576d4f2f4e16309207505da8ff3e6b34
Ondrej Spacek <ondrej.spacek@nxp.com>
2021-07-29 16:35:06 +0200

    ethernet: gmac: Allow gmac use the s32g platform serdes driver.
    This commit adds gmac specific callbacks that are called from Linux
    phylink_mac_ops. This should allow all possible operations of serdes
    on s32g for gmac.
    
    Possible operations:
    *SGMII AN phyless- managed = "in-band-status" in device tree
    *SGMII AN+PHY- managed = "in-band-status" and phy-handle in device tree
    *SGMII Fixed link- fixed-link in device tree, configured speed is
                       propagated to serdes
    *SGMII PHY- phy-handle in device tree, serdes is configured based on phy
    
    Issue: ALB-7210
    Signed-off-by: Ondrej Spacek <ondrej.spacek@nxp.com>

--- drivers/net/ethernet/stmicro/stmmac/dwmac-s32cc.c
+++ drivers/net/ethernet/stmicro/stmmac/dwmac-s32cc.c
@@ -12,10 +12,16 @@
 #include <linux/io.h>
 #include <linux/clk.h>
 #include <linux/phy.h>
+#include <linux/phylink.h>
+#include <linux/of_mdio.h>
 #include <linux/platform_device.h>
 #include <linux/of.h>
 #include <linux/of_net.h>
 #include <linux/stmmac.h>
+
+/* S32 Serdes */
+#include <linux/pcs/pcs-xpcs.h>
+#include <linux/pcs/fsl-s32gen1-xpcs.h>
 
 #include "stmmac_platform.h"
 
@@ -35,6 +41,65 @@ struct s32cc_priv_data {
 	phy_interface_t intf_mode;
 	struct clk *tx_clk;
 	struct clk *rx_clk;
+
+	/* Serdes */
+	int link_an;
+	bool phyless_an;
+	struct phy *serdes_phy;
+	struct s32gen1_xpcs *xpcs;
+	const struct s32gen1_xpcs_ops *xpcs_ops;
+};
+
+int xpcs_config(struct mdio_xpcs_args *xpcs,
+		const struct phylink_link_state *state)
+{
+	struct phylink_link_state sgmii_state = { 0 };
+	struct s32cc_priv_data *gmac = (struct s32cc_priv_data *)xpcs->bus;
+
+	if (gmac->intf_mode != PHY_INTERFACE_MODE_SGMII)
+		return 0;
+
+	if (!gmac->xpcs || !gmac->xpcs_ops)
+		return -EINVAL;
+
+	if (gmac->link_an == MLO_AN_FIXED || gmac->link_an == MLO_AN_PHY) {
+		gmac->xpcs_ops->xpcs_get_state(gmac->xpcs, &sgmii_state);
+		sgmii_state.speed = state->speed;
+		sgmii_state.duplex = state->duplex;
+		sgmii_state.an_enabled = false;
+		gmac->xpcs_ops->xpcs_config(gmac->xpcs, &sgmii_state);
+	} else if (gmac->link_an == MLO_AN_INBAND) {
+		gmac->xpcs_ops->xpcs_config(gmac->xpcs, state);
+	} else {
+		return -EINVAL;
+	}
+
+	return 0;
+}
+
+int xpcs_get_state(struct mdio_xpcs_args *xpcs,
+		   struct phylink_link_state *state)
+{
+	struct s32cc_priv_data *gmac = (struct s32cc_priv_data *)xpcs->bus;
+
+	if (gmac->intf_mode != PHY_INTERFACE_MODE_SGMII)
+		return 0;
+
+	if (!gmac->xpcs || !gmac->xpcs_ops)
+		return -EINVAL;
+
+	gmac->xpcs_ops->xpcs_get_state(gmac->xpcs, state);
+
+	/* Update SerDes state (Platform limitation) */
+	if (gmac->phyless_an)
+		gmac->xpcs_ops->xpcs_config(gmac->xpcs, state);
+
+	return 0;
+}
+
+static struct mdio_xpcs_ops s32cc_xpcs_ops = {
+	.config = xpcs_config,
+	.get_state = xpcs_get_state,
 };
 
 static int s32cc_gmac_init(struct platform_device *pdev, void *priv)
@@ -116,35 +181,101 @@ static void s32cc_fix_speed(void *priv, unsigned int speed)
 		return;
 
 	switch (speed) {
-			case SPEED_1000:
-				dev_info(gmac->dev, "Set RX/TX clock to 125M\n");
-				clk_set_rate(gmac->tx_clk, GMAC_TX_RATE_125M);
-				clk_set_rate(gmac->rx_clk, GMAC_TX_RATE_125M);
-				break;
-			case SPEED_100:
-				dev_info(gmac->dev, "Set RX/TX clock to 25M\n");
-				clk_set_rate(gmac->tx_clk, GMAC_TX_RATE_25M);
-				clk_set_rate(gmac->rx_clk, GMAC_TX_RATE_25M);
-				break;
-			case SPEED_10:
-				dev_info(gmac->dev, "Set RX/TX clock to 2.5M\n");
-				clk_set_rate(gmac->tx_clk, GMAC_TX_RATE_2M5);
-				clk_set_rate(gmac->rx_clk, GMAC_TX_RATE_2M5);
-				break;
-			default:
-				dev_err(gmac->dev, "Unsupported/Invalid speed: %d\n", speed);
-				return;
+	case SPEED_1000:
+		dev_info(gmac->dev, "Set RX/TX clock to 125M\n");
+		clk_set_rate(gmac->tx_clk, GMAC_TX_RATE_125M);
+		clk_set_rate(gmac->rx_clk, GMAC_TX_RATE_125M);
+		break;
+	case SPEED_100:
+		dev_info(gmac->dev, "Set RX/TX clock to 25M\n");
+		clk_set_rate(gmac->tx_clk, GMAC_TX_RATE_25M);
+		clk_set_rate(gmac->rx_clk, GMAC_TX_RATE_25M);
+		break;
+	case SPEED_10:
+		dev_info(gmac->dev, "Set RX/TX clock to 2.5M\n");
+		clk_set_rate(gmac->tx_clk, GMAC_TX_RATE_2M5);
+		clk_set_rate(gmac->rx_clk, GMAC_TX_RATE_2M5);
+		break;
+	default:
+		dev_err(gmac->dev, "Unsupported/Invalid speed: %d\n", speed);
+		return;
 	}
+}
+
+static int s32cc_configure_serdes(struct plat_stmmacenet_data *plat_dat,
+				  struct phy *serdes_phy)
+{
+	struct s32cc_priv_data *gmac = plat_dat->bsp_priv;
+	struct device_node *np = gmac->dev->of_node;
+	const char *managed;
+
+	gmac->serdes_phy = serdes_phy;
+
+	if (!gmac->serdes_phy) {
+		dev_err(gmac->dev, "SerDes PHY was not found\n");
+		return -EINVAL;
+	}
+
+	if (phy_init(gmac->serdes_phy) || phy_power_on(gmac->serdes_phy)) {
+		dev_err(gmac->dev, "SerDes PHY init failed\n");
+		return -EINVAL;
+	}
+
+	if (phy_configure(gmac->serdes_phy, NULL)) {
+		dev_err(gmac->dev, "SerDes PHY configuration failed\n");
+		return -EINVAL;
+	}
+
+	gmac->xpcs = s32gen1_phy2xpcs(gmac->serdes_phy);
+	gmac->xpcs_ops = s32gen1_xpcs_get_ops();
+
+	/* We have to know interface type due to platform limitations */
+	gmac->link_an = MLO_AN_PHY;
+	if (of_phy_is_fixed_link(np))
+		gmac->link_an = MLO_AN_FIXED;
+
+	if (of_property_read_string(np, "managed", &managed) == 0 &&
+	    strcmp(managed, "in-band-status") == 0) {
+		gmac->link_an = MLO_AN_INBAND;
+		dev_info(gmac->dev, "SGMII AN enabled\n");
+	}
+
+	gmac->phyless_an = false;
+	if (gmac->link_an == MLO_AN_INBAND &&
+	    !of_parse_phandle(np, "phy-handle", 0)) {
+		dev_info(gmac->dev, "PHY less SGMII\n");
+		gmac->phyless_an = true;
+	}
+
+	if (!gmac->xpcs || !gmac->xpcs_ops) {
+		dev_err(gmac->dev, "Can't get SGMII PCS\n");
+		gmac->xpcs_ops = NULL;
+		gmac->xpcs = NULL;
+	}
+
+	return 0;
 }
 
 static int s32cc_dwmac_probe(struct platform_device *pdev)
 {
 	struct plat_stmmacenet_data *plat_dat;
+	struct stmmac_priv *priv;
 	struct stmmac_resources stmmac_res;
 	struct s32cc_priv_data *gmac;
 	struct resource *res;
 	const char *tx_clk, *rx_clk;
+	struct phy *serdes_phy = NULL;
 	int ret;
+
+	if (device_get_phy_mode(&pdev->dev) == PHY_INTERFACE_MODE_SGMII) {
+		serdes_phy = devm_phy_get(&pdev->dev, "gmac_xpcs");
+
+		if (IS_ERR(serdes_phy) &&
+		    (PTR_ERR(serdes_phy) == -EPROBE_DEFER))
+			return -EPROBE_DEFER;
+		else if (IS_ERR(serdes_phy))
+			serdes_phy = NULL;
+	}
 
 	ret = stmmac_get_platform_resources(pdev, &stmmac_res);
 	if (ret)
@@ -154,6 +285,7 @@ static int s32cc_dwmac_probe(struct platform_device *pdev)
 	if (!gmac) {
 		return PTR_ERR(gmac);
 	}
+
 	gmac->dev = &pdev->dev;
 
 	/* S32G control reg */
@@ -164,21 +296,20 @@ static int s32cc_dwmac_probe(struct platform_device *pdev)
 		return PTR_ERR(gmac->ctrl_sts);
 	}
 
-	/* phy mode */
-	ret = of_get_phy_mode(pdev->dev.of_node, &gmac->intf_mode);
-	if (ret) {
-		dev_err(&pdev->dev, "phy-mode property is missing\n");
-		return ret;
-	}
+	plat_dat = stmmac_probe_config_dt(pdev, &stmmac_res.mac);
+	if (IS_ERR(plat_dat))
+		return PTR_ERR(plat_dat);
 
-	if (gmac->intf_mode != PHY_INTERFACE_MODE_SGMII &&
-	    !phy_interface_mode_is_rgmii(gmac->intf_mode)) {
+	plat_dat->bsp_priv = gmac;
+
+	if (plat_dat->phy_interface != PHY_INTERFACE_MODE_SGMII &&
+	    !phy_interface_mode_is_rgmii(plat_dat->phy_interface)) {
 		dev_err(&pdev->dev, "Not supported phy interface mode: [%s]\n",
-			phy_modes(gmac->intf_mode));
+			phy_modes(plat_dat->phy_interface));
 		return -EINVAL;
 	}
 
-	switch (gmac->intf_mode) {
+	switch (plat_dat->phy_interface) {
 	case PHY_INTERFACE_MODE_SGMII:
 		tx_clk = "tx_sgmii";
 		rx_clk = "rx_sgmii";
@@ -201,9 +332,7 @@ static int s32cc_dwmac_probe(struct platform_device *pdev)
 		break;
 	};
 
-	plat_dat = stmmac_probe_config_dt(pdev, &stmmac_res.mac);
-	if (IS_ERR(plat_dat))
-		return PTR_ERR(plat_dat);
+	gmac->intf_mode = plat_dat->phy_interface;
 
 	plat_dat->bus_id = of_alias_get_id(pdev->dev.of_node, "gmac");
 	if (plat_dat->bus_id < 0)
@@ -215,6 +344,10 @@ static int s32cc_dwmac_probe(struct platform_device *pdev)
 		goto err_remove_config_dt;
 	}
 
+	ret = s32cc_configure_serdes(plat_dat, serdes_phy);
+	if (ret)
+		dev_err(&pdev->dev, "SERDES is not configured\n");
+
 	/* tx clock */
 	gmac->tx_clk = devm_clk_get(&pdev->dev, tx_clk);
 	if (IS_ERR(gmac->tx_clk)) {
@@ -225,15 +358,13 @@ static int s32cc_dwmac_probe(struct platform_device *pdev)
 	/* rx clock */
 	gmac->rx_clk = devm_clk_get(&pdev->dev, rx_clk);
 	if (IS_ERR(gmac->rx_clk)) {
-		dev_info(&pdev->dev, "tx clock not found\n");
+		dev_info(&pdev->dev, "rx clock not found\n");
 		gmac->rx_clk = NULL;
 	}
 
 	ret = s32cc_gmac_init(pdev, gmac);
 	if (ret)
 		goto err_remove_config_dt;
-
-	plat_dat->bsp_priv = gmac;
 
 	/* core feature set */
 	plat_dat->has_gmac4 = true;
@@ -242,7 +373,6 @@ static int s32cc_dwmac_probe(struct platform_device *pdev)
 
 	plat_dat->init = s32cc_gmac_init;
 	plat_dat->exit = s32cc_gmac_exit;
-	plat_dat->bsp_priv = gmac;
 	plat_dat->fix_mac_speed = s32cc_fix_speed;
 
 	/* configure bitfield for quirks */
@@ -255,6 +385,15 @@ static int s32cc_dwmac_probe(struct platform_device *pdev)
 	if (ret)
 		goto err_gmac_exit;
 
+	/* This is little hacked as we do not have
+	 * the serdes accessible via MDIO
+	 */
+	if (gmac->xpcs || gmac->xpcs_ops) {
+		priv = netdev_priv(dev_get_drvdata(&pdev->dev));
+		priv->hw->xpcs = &s32cc_xpcs_ops;
+		priv->hw->xpcs_args.bus = (struct mii_bus *)gmac;
+	}
+
 	return 0;
 
 err_gmac_exit:
@@ -262,6 +401,23 @@ err_gmac_exit:
 err_remove_config_dt:
 	stmmac_remove_config_dt(pdev, plat_dat);
 	return ret;
+}
+
+static int s32cc_dwmac_remove(struct platform_device *pdev)
+{
+	struct net_device *ndev = platform_get_drvdata(pdev);
+	struct stmmac_priv *priv = netdev_priv(ndev);
+	struct plat_stmmacenet_data *plat = priv->plat;
+	struct s32cc_priv_data *gmac = plat->bsp_priv;
+
+	if (gmac->serdes_phy) {
+		phy_exit(gmac->serdes_phy);
+		gmac->serdes_phy = NULL;
+		gmac->xpcs = NULL;
+		gmac->xpcs_ops = NULL;
+	}
+
+	return stmmac_pltfr_remove(pdev);
 }
 
 static const struct of_device_id s32_dwmac_match[] = {
@@ -272,7 +428,7 @@ MODULE_DEVICE_TABLE(of, s32_dwmac_match);
 
 static struct platform_driver s32_dwmac_driver = {
 	.probe  = s32cc_dwmac_probe,
-	.remove = stmmac_pltfr_remove,
+	.remove = s32cc_dwmac_remove,
 	.driver = {
 		.name           = "s32cc-dwmac",
 		.pm		= &stmmac_pltfr_pm_ops,
