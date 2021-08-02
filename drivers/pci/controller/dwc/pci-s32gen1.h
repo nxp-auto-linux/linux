aed3df5e9269686e4dda505e1b82ca67b6f96f02
Ciprian Marian Costea <ciprianmarian.costea@nxp.com>
2021-08-02 13:31:49 +0300

    s32g: serdes: Add SRIS clock mode
    Issue: ALB-6998
    Signed-off-by: Ciprian Marian Costea <ciprianmarian.costea@nxp.com>

--- drivers/pci/controller/dwc/pci-s32gen1.h
+++ drivers/pci/controller/dwc/pci-s32gen1.h
@@ -13,6 +13,7 @@
 #include <linux/types.h>
 #include <linux/version.h>
 #include <linux/phy/phy.h>
+#include <linux/pcie/fsl-s32gen1-pcie-phy-submode.h>
 #include "pcie-designware.h"
 
 #define BUILD_BIT_VALUE(field, x) (((x) & (1)) << field##_BIT)
@@ -37,6 +38,8 @@
 #define   DEVICE_TYPE_MASK		(0x0000000F)
 #define   DEVICE_TYPE			((DEVICE_TYPE_MASK) << \
 					(DEVICE_TYPE_LSB))
+#define   SRIS_MODE_BIT			(8)
+#define   SRIS_MODE_MASK		BIT(SRIS_MODE_BIT)
 
 #define PCI_EXP_CAP_ID_OFFSET	0x70
 
@@ -91,6 +94,7 @@ struct s32gen1_pcie {
 	void __iomem *atu_base;
 
 	int id;
+	enum pcie_phy_mode phy_mode;
 	enum pcie_link_speed linkspeed;
 
 #ifdef CONFIG_PCI_DW_DMA
