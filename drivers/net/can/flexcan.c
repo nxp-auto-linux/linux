2db038e570e07659477923f580754ea835d73c11
Ciprian Marian Costea <ciprianmarian.costea@nxp.com>
2021-08-17 14:31:08 +0300

    s32gen1: flexcan: Fix unexpected unlock
    Before this change, in 'flexcan_mailbox_read' if the message was
    dropped in the first check, execution will go to 'mark_as_read' label,
    where it will unlock a never previously acquired lock.
    
    Issue: ALB-7527
    Signed-off-by: Ciprian Marian Costea <ciprianmarian.costea@nxp.com>

--- drivers/net/can/flexcan.c
+++ drivers/net/can/flexcan.c
@@ -1180,6 +1180,7 @@ static struct sk_buff *flexcan_mailbox_read(struct can_rx_offload *offload,
 
 	if (unlikely(drop)) {
 		skb = ERR_PTR(-ENOBUFS);
+		spin_lock_irqsave(&priv->timer_access, flags);
 		goto mark_as_read;
 	}
 
