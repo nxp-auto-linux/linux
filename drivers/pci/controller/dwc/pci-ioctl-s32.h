5d59b0d1664254e238e2dd11da37bad5d87134dc
Ionut Vicovan <Ionut.Vicovan@nxp.com>
2021-09-29 11:59:18 +0000

    s32:dma: Separate DMA code from ioctl code
    Issue: ALB-7866
    
    Signed-off-by: Ionut Vicovan <Ionut.Vicovan@nxp.com>

--- drivers/pci/controller/dwc/pci-ioctl-s32.h
+++ drivers/pci/controller/dwc/pci-ioctl-s32.h
@@ -32,16 +32,6 @@
 #define SEND_LL				_IOWR('S', 13, struct dma_list(*)[])
 #define START_LL			_IOWR('S', 14, unsigned int)
 
-#define DMA_FLAG_LIE					(1 << 0)
-#define DMA_FLAG_RIE					(1 << 1)
-#define DMA_FLAG_LLP					(1 << 2)
-#define DMA_FLAG_WRITE_ELEM				(1 << 3)
-#define DMA_FLAG_READ_ELEM				(1 << 4)
-#define DMA_FLAG_EN_DONE_INT			(1 << 5)
-#define DMA_FLAG_EN_ABORT_INT			(1 << 6)
-#define DMA_FLAG_EN_REMOTE_DONE_INT		(1 << 7)
-#define DMA_FLAG_EN_REMOTE_ABORT_INT	(1 << 8)
-
 struct s32_inbound_region {
 	unsigned int  bar_nr;
 	unsigned int  target_addr;
@@ -60,31 +50,6 @@ struct s32_bar {
 	unsigned int bar_nr;
 	unsigned int size;
 	unsigned int addr;
-};
-
-/* Single block DMA transfer struct */
-struct dma_data_elem {
-	unsigned long sar;
-	unsigned long dar;
-	unsigned long imwr;
-	unsigned int size;
-	unsigned int flags;
-	unsigned int ch_num;
-};
-/* Type of array of structures for passing linked list  */
-struct dma_list {
-	unsigned long sar;
-	unsigned long dar;
-	unsigned int size;
-};
-
-/* Linked list mode struct */
-struct dma_ll_info {
-	unsigned int direction;
-	unsigned int ch_num;
-	unsigned int nr_elem;
-	unsigned int phy_list_addr;
-	unsigned int next_phy_list_addr;
 };
 
 struct s32_userspace_info;
@@ -106,7 +71,6 @@ void s32_config_user_space_data(struct s32_userspace_info *uinfo,
  * accessible at this level
  */
 
-struct dma_info *dw_get_dma_info(struct dw_pcie *pcie);
 struct s32_userspace_info *dw_get_userspace_info(struct dw_pcie *pcie);
 
 int s32_pcie_setup_outbound(struct s32_outbound_region *outbStr);
