8aab2c7ac5c1d9245dffff086b822c73a647d2ce
Bogdan Folea <bogdan.folea@nxp.com>
2021-10-07 20:54:10 +0300

    crypto: hse: minor refactor and fix iomem deref
    ALB-7530
    Signed-off-by: Bogdan Folea <bogdan.folea@nxp.com>

--- drivers/crypto/hse/hse-mu.h
+++ drivers/crypto/hse/hse-mu.h
@@ -40,9 +40,11 @@ enum hse_irq_type {
 	HSE_INT_SYS_EVENT = 2u,
 };
 
-void *hse_mu_init(struct device *dev, void **desc_base_ptr, u64 *desc_base_dma,
-		  irqreturn_t (*rx_isr)(int irq, void *dev),
-		  irqreturn_t (*event_isr)(int irq, void *dev));
+void *hse_mu_init(struct device *dev, irqreturn_t (*rx_isr)(int irq, void *dev),
+		  irqreturn_t (*evt_isr)(int irq, void *dev));
+
+void __iomem *hse_mu_desc_base_ptr(void *mu);
+dma_addr_t hse_mu_desc_base_dma(void *mu);
 
 u16 hse_mu_check_status(void *mu);
 u32 hse_mu_check_event(void *mu);
