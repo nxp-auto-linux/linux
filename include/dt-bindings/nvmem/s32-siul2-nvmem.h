1378039d5e1dfd75a4d5e824818a62d1761c4814
Ciprian Marian Costea <ciprianmarian.costea@nxp.com>
2021-11-15 11:39:38 +0200

    s32-gen1: nvmem: Create include file for nvmem cells
    Issue: ALB-8072
    Signed-off-by: Ciprian Marian Costea <ciprianmarian.costea@nxp.com>

---
+++ include/dt-bindings/nvmem/s32-siul2-nvmem.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright 2021 NXP
+ */
+
+/* Start address of SIUL2_0 module */
+#define SOC_REVISION_OFFSET		(0)
+#define SOC_REVISION_SIZE		(4)
+
+/* Start address of SIUL2_1 module */
+#define SERDES_PRESENCE_OFFSET (0x0)
+#define SERDES_PRESENCE_SIZE   (0x4)
