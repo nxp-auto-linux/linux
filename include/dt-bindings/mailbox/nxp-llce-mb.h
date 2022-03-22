d1d34f65a7d7550c8cfc86233f58e8868246535f
Radu Pirea (NXP OSS) <radu-nicolae.pirea@oss.nxp.com>
2022-03-22 12:03:21 +0200

    dt-bindings: llce-mailbox: add aux channel
    Issue: ALB-8659
    
    Signed-off-by: Radu Pirea (NXP OSS) <radu-nicolae.pirea@oss.nxp.com>

--- include/dt-bindings/mailbox/nxp-llce-mb.h
+++ include/dt-bindings/mailbox/nxp-llce-mb.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 /*
- * Copyright 2020-2021 NXP
+ * Copyright 2020-2022 NXP
  */
 #ifndef _DT_BINDINGS_MAILBOX_NXP_LLCE_MB_H_
 #define _DT_BINDINGS_MAILBOX_NXP_LLCE_MB_H_
@@ -13,5 +13,6 @@
 #define S32G_LLCE_CAN_RX_MB		0x2
 #define S32G_LLCE_CAN_TX_MB		0x3
 #define S32G_LLCE_CAN_LOGGER_MB		0x4
+#define S32G_LLCE_CAN_LOGGER_CONFIG_MB	0x5
 
 #endif
