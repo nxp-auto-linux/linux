ab7af92189e62357e3422639b3a37fa60cdec279
Ciprian Costea <ciprianmarian.costea@nxp.com>
2022-03-28 13:27:41 +0300

    s32: nvmem: Define new nvram offset for PCIe variant bits
    Issue: ALB-8078
    Signed-off-by: Ciprian Costea <ciprianmarian.costea@nxp.com>

--- include/dt-bindings/nvmem/s32-siul2-nvmem.h
+++ include/dt-bindings/nvmem/s32-siul2-nvmem.h
@@ -1,12 +1,13 @@
 /* SPDX-License-Identifier: GPL-2.0+ */
 /*
- * Copyright 2021 NXP
+ * Copyright 2021-2022 NXP
  */
 
-/* Start address of SIUL2_0 module */
-#define SOC_REVISION_OFFSET		(0)
-#define SOC_REVISION_SIZE		(4)
+/* NVRAM offset and size for SIUL2_0 module */
+#define SOC_REVISION_OFFSET		(0x0)
+#define PCIE_VARIANT_OFFSET		(0x4)
 
-/* Start address of SIUL2_1 module */
-#define SERDES_PRESENCE_OFFSET (0x0)
-#define SERDES_PRESENCE_SIZE   (0x4)
+/* NVRAM offset and size for SIUL2_1 module */
+#define SERDES_PRESENCE_OFFSET	(0x0)
+
+#define NVRAM_CELL_SIZE			(0x4)
