bee33a4d35f8a858b46986e1012ea149ca2041b9
Jan Petrous <jan.petrous@nxp.com>
2022-04-25 12:34:32 +0200

    dt-bindings: net: pfe: add dt bindings documentation
    Redesigned pfeng binding for driver RTM 1.0.0.
    Converted to YAML.
    
    Issue: AAVB-4028
    Signed-off-by: Jan Petrous <jan.petrous@nxp.com>

---
+++ include/dt-bindings/net/s32g-pfe.h
@@ -0,0 +1,20 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright 2021-2022 NXP
+ *
+ */
+
+#ifndef __DT_BINDINGS_NET_S32G_PFE_H__
+#define __DT_BINDINGS_NET_S32G_PFE_H__
+
+#define PFE_HIF_CHANNEL_0		0
+#define PFE_HIF_CHANNEL_1		1
+#define PFE_HIF_CHANNEL_2		2
+#define PFE_HIF_CHANNEL_3		3
+#define PFE_HIF_CHANNEL_NOCPY		16
+
+#define PFE_EMAC_0			0
+#define PFE_EMAC_1			1
+#define PFE_EMAC_2			2
+
+#endif /* __DT_BINDINGS_NET_S32G_PFE_H__ */
