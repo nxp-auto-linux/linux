0de46211860a95fd3ea1d4ecb718b1e8ed9e19d4
Ghennadi Procopciuc <ghennadi.procopciuc@nxp.com>
2022-05-02 13:55:13 +0300

    i2c: Update S32CC compatible to nxp,s32cc-i2c
    Issue: ALB-8823
    Signed-off-by: Ghennadi Procopciuc <ghennadi.procopciuc@nxp.com>

--- drivers/i2c/busses/i2c-imx.c
+++ drivers/i2c/busses/i2c-imx.c
@@ -17,7 +17,7 @@
  *	Copyright (C) 2008 Darius Augulis <darius.augulis at teltonika.lt>
  *
  *	Copyright 2013 Freescale Semiconductor, Inc.
- *	Copyright 2017, 2021 NXP
+ *	Copyright 2017, 2021-2022 NXP
  */
 
 #include <linux/acpi.h>
@@ -291,7 +291,7 @@ static const struct of_device_id i2c_imx_dt_ids[] = {
 	{ .compatible = "fsl,imx1-i2c", .data = &imx1_i2c_hwdata, },
 	{ .compatible = "fsl,imx21-i2c", .data = &imx21_i2c_hwdata, },
 	{ .compatible = "fsl,vf610-i2c", .data = &vf610_i2c_hwdata, },
-	{ .compatible = "fsl,s32gen1-i2c", .data = &s32_i2c_hwdata, },
+	{ .compatible = "nxp,s32cc-i2c", .data = &s32_i2c_hwdata, },
 	{ /* sentinel */ }
 };
 MODULE_DEVICE_TABLE(of, i2c_imx_dt_ids);
