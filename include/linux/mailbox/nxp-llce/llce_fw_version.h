8fb806359d7ef1100002098b11a880635f79c06d
Radu Pirea (NXP OSS) <radu-nicolae.pirea@oss.nxp.com>
2022-05-16 17:30:21 +0300

    mailbox: llce: upgrade to LLCE 1.0.4
    Issue: ALB-8892
    
    Signed-off-by: Radu Pirea (NXP OSS) <radu-nicolae.pirea@oss.nxp.com>

--- include/linux/mailbox/nxp-llce/llce_fw_version.h
+++ include/linux/mailbox/nxp-llce/llce_fw_version.h
@@ -1,8 +1,20 @@
 /* SPDX-License-Identifier: GPL-2.0+ */
-/* Copyright 2020-2021 NXP */
+/* Copyright 2020-2022 NXP */
 #ifndef LLCE_FW_VERSION_H
 #define LLCE_FW_VERSION_H
 
 #define LLCE_VERSION_MAX_LENGTH 50
+
+/**
+ * Firmware version datatype.
+ * It contains the version_string which encodes the
+ * target derivative and the versions of each feature
+ **/
+struct llce_fw_version {
+	/** OUTPUT: LLCE FW version string actual length. */
+	u8 string_length;
+	/** OUTPUT: LLCE FW version string. */
+	u8 version_string[LLCE_VERSION_MAX_LENGTH];
+} __aligned(4) __packed;
 
 #endif /* LLCE_FW_VERSION_H*/
