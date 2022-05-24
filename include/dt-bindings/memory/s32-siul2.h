773542e019219117412a7089710fb9fb23191f51
Andrei Stefanescu <andrei.stefanescu@nxp.com>
2022-05-24 15:04:23 +0300

    dt-bindings: s32: add siul2 definitions
    These definitions are used for describing the memory ranges of SIUL2.
    
    Issue: ALB-8239
    
    Signed-off-by: Andrei Stefanescu <andrei.stefanescu@nxp.com>

---
+++ include/dt-bindings/memory/s32-siul2.h
@@ -0,0 +1,23 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later OR BSD-3-Clause */
+/*
+ * Copyright 2022 NXP
+ */
+
+#ifndef __DT_BINDINGS_S32_SIUL2_MEMORY_H__
+#define __DT_BINDINGS_S32_SIUL2_MEMORY_H__
+
+#define MIDR_SIUL2_0		(0)
+#define MIDR_SIUL2_1		(1)
+#define MSCRS_SIUL2_0		(2)
+#define MSCRS_SIUL2_1		(3)
+#define IMCRS_SIUL2_0		(4)
+#define IMCRS_SIUL2_1_0		(5)
+#define IMCRS_SIUL2_1_EIRQS	(6)
+#define IMCRS_SIUL2_1_1		(7)
+#define OPADS_SIUL2_0		(8)
+#define OPADS_SIUL2_1		(9)
+#define IPADS_SIUL2_0		(10)
+#define IPADS_SIUL2_1		(11)
+#define EIRQS_SIUL2_1		(12)
+
+#endif /*  __DT_BINDINGS_S32_SIUL2_MEMORY_H__ */
