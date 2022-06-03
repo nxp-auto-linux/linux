4774f1a9365b2f3ad66f48600d985a14228edb2e
Ionut Vicovan <Ionut.Vicovan@nxp.com>
2022-06-03 15:19:37 +0300

    s32g3: pcie: Set correct PCI device ID and vendor ID
    For S32G3, the PCI vendor ID which is written by default in
    register DEVICE_ID_VENDOR_ID_REG (dbi + 0x0) is 0x16C3, which
    means "Synopsys". It should be 0x1957 ("Freescale").
    This has to be corrected manually.
    Also, reading the device ID from SIUL2 registers does not alwais have
    the expected results (again, it was the case with some G3 chips) and
    may occur with other parts as well.
    To avoid this, allow usage of a new device tree property (pcie_device_id)
    for setting the correct device ID from outside (e.g. bootloader).
    
    Issue: ALB-8914
    
    Signed-off-by: Ionut Vicovan <Ionut.Vicovan@nxp.com>

--- drivers/pci/controller/dwc/pci-s32gen1.c
+++ drivers/pci/controller/dwc/pci-s32gen1.c
@@ -1211,7 +1211,7 @@ static int s32gen1_pcie_dt_init(struct platform_device *pdev,
 	const struct s32gen1_pcie_data *data;
 	enum dw_pcie_device_mode mode;
 	struct dw_pcie_ep *ep = &pcie->ep;
-	u32 pcie_vendor_id, pcie_variant_bits = 0;
+	u32 pcie_vendor_id = PCI_VENDOR_ID_FREESCALE, pcie_variant_bits = 0;
 	int ret;
 #ifndef CONFIG_PCI_S32GEN1_IOCTL_LIMIT_ONE_ENDPOINT
 	struct device_node *shmn;
@@ -1333,15 +1333,21 @@ static int s32gen1_pcie_dt_init(struct platform_device *pdev,
 	if (!s32_pp->is_endpoint && of_parse_phandle(np, "msi-parent", 0))
 		s32_pp->has_msi_parent = true;
 
-	ret = s32_siul2_nvmem_get_pcie_dev_id(dev, "pcie_variant",
+	ret = of_property_read_u32(np, "pcie_device_id", &pcie_variant_bits);
+	if (ret) {
+		ret = s32_siul2_nvmem_get_pcie_dev_id(dev, "pcie_variant_bits",
 					      &pcie_variant_bits);
-	if (ret)
-		return ret;
+		if (ret || !pcie_variant_bits) {
+			dev_info(dev, "Error reading SIUL2 Device ID\n");
+			return ret;
+		}
+	}
 
 	dw_pcie_dbi_ro_wr_en(pcie);
-	pcie_vendor_id = dw_pcie_readl_dbi(pcie, PCI_VENDOR_ID);
 	pcie_vendor_id |= pcie_variant_bits << PCI_DEVICE_ID_SHIFT;
-	dev_info(dev, "Setting PCI Device ID to: 0x%x\n", pcie_vendor_id >> 16);
+	dev_info(dev, "Setting PCI Device and Vendor IDs to 0x%x:0x%x\n",
+		     (u32)(pcie_vendor_id >> PCI_DEVICE_ID_SHIFT),
+		     (u32)(pcie_vendor_id & GENMASK(15, 0)));
 	dw_pcie_writel_dbi(pcie, PCI_VENDOR_ID, pcie_vendor_id);
 	dw_pcie_dbi_ro_wr_dis(pcie);
 
