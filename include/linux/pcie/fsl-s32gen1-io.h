e4438eed38a61edea74f67e97d910159ea1efd9b
Ionut Vicovan <Ionut.Vicovan@nxp.com>
2022-08-17 01:36:39 +0300

    s32cc: Add io bit operations similar to U-boot
    This header file adds functions for setting, clearing and mixed
    bit operations.
    The code is taken from U-Boot (arch/arm/include/asm/io.h) but can
    also be found in Linux for the PPC architecture.
    
    Issue: ALB-9179
    
    Signed-off-by: Ionut Vicovan <Ionut.Vicovan@nxp.com>

---
+++ include/linux/pcie/fsl-s32gen1-io.h
@@ -0,0 +1,35 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/**
+ * Copyright 2022 NXP
+ *
+ * Bit operations. From u-boot: arch/arm/include/asm/io.h
+ */
+
+#ifndef FSL_S32GEN1_IO_H
+#define FSL_S32GEN1_IO_H
+
+#include <linux/io.h>
+
+#define clrbits(type, addr, clear) \
+	__raw_write##type(__raw_read##type(addr) & ~(clear), (addr))
+
+#define setbits(type, addr, set) \
+	__raw_write##type(__raw_read##type(addr) | (set), (addr))
+
+#define clrsetbits(type, addr, clear, set) \
+	__raw_write##type((__raw_read##type(addr) & ~(clear)) | (set), (addr))
+
+
+#define clrbits_32(addr, clear) clrbits(l, addr, clear)
+#define setbits_32(addr, set) setbits(l, addr, set)
+#define clrsetbits_32(addr, clear, set) clrsetbits(l, addr, clear, set)
+
+#define clrbits_16(addr, clear) clrbits(w, addr, clear)
+#define setbits_16(addr, set) setbits(w, addr, set)
+#define clrsetbits_16(addr, clear, set) clrsetbits(w, addr, clear, set)
+
+#define clrbits_8(addr, clear) clrbits(b, addr, clear)
+#define setbits_8(addr, set) setbits(b, addr, set)
+#define clrsetbits_8(addr, clear, set) clrsetbits(b, addr, clear, set)
+
+#endif /* FSL_S32GEN1_IO_H*/
