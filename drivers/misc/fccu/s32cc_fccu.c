c7512bcb4a93ee409bb605430eda6749846bbea8
Ciprian Costea <ciprianmarian.costea@nxp.com>
2022-08-19 12:20:23 +0300

    s32cc: fccu: Add STR support
    Issue: ALB-6347
    Issue: ALB-6949
    Issue: ALB-9104
    Signed-off-by: Ciprian Costea <ciprianmarian.costea@nxp.com>

--- drivers/misc/fccu/s32cc_fccu.c
+++ drivers/misc/fccu/s32cc_fccu.c
@@ -2,7 +2,7 @@
 /*
  * FCCU driver for S32CC SoC
  *
- * Copyright 2018 NXP.
+ * Copyright 2018, 2021 NXP.
  *
  * Drives the Fault Collection and Control Unit.
  *
@@ -291,6 +291,26 @@ static int __exit fccu_remove(struct platform_device *pdev)
 	return 0;
 }
 
+static int __maybe_unused fccu_suspend(struct device *dev)
+{
+	return 0;
+}
+
+static int __maybe_unused fccu_resume(struct device *dev)
+{
+	struct fccu_pri_data_t *priv_data = dev_get_drvdata(dev);
+	int ret;
+
+	ret = enable_swt_rs_chanel(dev, priv_data->base);
+	if (ret)
+		dev_err(dev, "%s, %d, configuration meet timeout\n",
+			__func__, __LINE__);
+
+	return ret;
+}
+
+static SIMPLE_DEV_PM_OPS(fccu_pm_ops, fccu_suspend, fccu_resume);
+
 static const struct of_device_id fccu_dt_ids[] = {
 	{.compatible = "nxp,s32cc-fccu",},
 	{ /* sentinel */ }
@@ -302,7 +322,8 @@ static struct platform_driver fccu_driver = {
 			.name = DRIVER_NAME,
 			.owner = THIS_MODULE,
 			.of_match_table = fccu_dt_ids,
-			},
+			.pm = &fccu_pm_ops,
+		},
 };
 
 module_platform_driver_probe(fccu_driver, fccu_probe);
