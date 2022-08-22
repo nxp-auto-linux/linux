ccf3350c893bf532be134a3c5422812f3b1c2b01
Ciprian Marian Costea <ciprianmarian.costea@nxp.com>
2022-08-22 15:37:13 +0300

    s32cc: linflex: Fix static locking context imbalance
    Issue: ALB-7523
    Issue: ALB-9101
    Signed-off-by: Ciprian Marian Costea <ciprianmarian.costea@nxp.com>

--- drivers/tty/serial/fsl_linflexuart.c
+++ drivers/tty/serial/fsl_linflexuart.c
@@ -1307,17 +1307,14 @@ linflex_console_write(struct console *co, const char *s, unsigned int count)
 {
 	struct uart_port *sport = linflex_ports[co->index];
 	unsigned long flags;
-	int locked = 1;
 
-	if (sport->sysrq)
-		locked = 0;
-	else
+	if (sport->sysrq) {
+		linflex_string_write(sport, s, count);
+	} else {
 		spin_lock_irqsave(&sport->lock, flags);
-
-	linflex_string_write(sport, s, count);
-
-	if (locked)
+		linflex_string_write(sport, s, count);
 		spin_unlock_irqrestore(&sport->lock, flags);
+	}
 }
 
 /*
