3b07d4c25c0fb0b427c5b7f764b40a0652b6b902
Ciprian Costea <ciprianmarian.costea@nxp.com>
2022-08-31 12:15:53 +0300

    arm64: dts: Add SIUL2 modules for S32CC platforms
    Issue: ALB-8239
    Issue: ALB-8823
    Issue: ALB-9096
    Signed-off-by: Andrei Stefanescu <andrei.stefanescu@nxp.com>
    Signed-off-by: Ghennadi Procopciuc <ghennadi.procopciuc@nxp.com>
    Signed-off-by: Ciprian Costea <ciprianmarian.costea@nxp.com>

---
+++ include/dt-bindings/memory/s32cc-siul2.h
@@ -0,0 +1,25 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later OR BSD-3-Clause */
+/*
+ * Copyright 2022 NXP
+ */
+
+#ifndef __DT_BINDINGS_S32CC_SIUL2_MEMORY_H__
+#define __DT_BINDINGS_S32CC_SIUL2_MEMORY_H__
+
+#define MIDR_SIUL2_0		(0)
+#define MIDR_SIUL2_1		(1)
+#define MSCRS_SIUL2_0		(2)
+#define MSCRS_SIUL2_1		(3)
+#define MSCRS_SIUL2_1_0		(4)
+#define MSCRS_SIUL2_1_1		(5)
+#define IMCRS_SIUL2_0		(6)
+#define IMCRS_SIUL2_1_0		(7)
+#define IMCRS_SIUL2_1_EIRQS	(8)
+#define IMCRS_SIUL2_1_1		(9)
+#define OPADS_SIUL2_0		(10)
+#define OPADS_SIUL2_1		(11)
+#define IPADS_SIUL2_0		(12)
+#define IPADS_SIUL2_1		(13)
+#define EIRQS_SIUL2_1		(14)
+
+#endif /*  __DT_BINDINGS_S32CC_SIUL2_MEMORY_H__ */
