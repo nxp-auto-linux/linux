242a87c1f3ab01b2757f72b94f6490a936cfe710
Ciprian Costea <ciprianmarian.costea@nxp.com>
2022-09-01 10:09:50 +0300

    arm64: dts: Add LLCE entries for S32G platforms
    Issue: ALB-4265
    Issue: ALB-5829
    Issue: ALB-5830
    Issue: ALB-7344
    Issue: ALB-7505
    Issue: ALB-7935
    Issue: ALB-8375
    Issue: ALB-8659
    Issue: ALB-8823
    Issue: ALB-9096
    Signed-off-by: Dorin Ionita <dorin.ionita@nxp.com>
    Signed-off-by: Ghennadi Procopciuc <Ghennadi.Procopciuc@nxp.com>
    Signed-off-by: Radu Pirea <radu-nicolae.pirea@nxp.com>

---
+++ include/dt-bindings/mailbox/nxp-llce-mb.h
@@ -0,0 +1,18 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright 2020-2022 NXP
+ */
+#ifndef _DT_BINDINGS_MAILBOX_NXP_LLCE_MB_H_
+#define _DT_BINDINGS_MAILBOX_NXP_LLCE_MB_H_
+
+/*
+ * LLCE Mailbox Type
+ */
+#define S32G_LLCE_HIF_CONF_MB		0x0
+#define S32G_LLCE_CAN_CONF_MB		0x1
+#define S32G_LLCE_CAN_RX_MB		0x2
+#define S32G_LLCE_CAN_TX_MB		0x3
+#define S32G_LLCE_CAN_LOGGER_MB		0x4
+#define S32G_LLCE_CAN_LOGGER_CONFIG_MB	0x5
+
+#endif
