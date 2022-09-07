93311323bcfdfc3973146457e1ac417974a9b1d2
Ghennadi Procopciuc <ghennadi.procopciuc@nxp.com>
2022-09-07 10:17:58 +0300

    bindings/reset: Add S32G3 SCMI reset controller IDs
    Issue: ALB-7721
    Issue: ALB-9109
    Signed-off-by: Ghennadi Procopciuc <ghennadi.procopciuc@nxp.com>

---
+++ include/dt-bindings/reset/s32g3-scmi-reset.h
@@ -0,0 +1,24 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/*
+ * Copyright 2021 NXP
+ */
+#ifndef __DT_BINDINGS_S32G3_SCMI_RESET_H
+#define __DT_BINDINGS_S32G3_SCMI_RESET_H
+
+#include <dt-bindings/reset/s32g-scmi-reset.h>
+
+#define S32G3_SCMI_RST(N)	((N) + S32G_SCMI_RST_MAX_ID)
+
+#define S32G3_SCMI_RST_CM7_3	S32G3_SCMI_RST(0)
+#define S32G3_SCMI_RST_A53_4	S32G3_SCMI_RST(1)
+#define S32G3_SCMI_RST_A53_5	S32G3_SCMI_RST(2)
+#define S32G3_SCMI_RST_A53_6	S32G3_SCMI_RST(3)
+#define S32G3_SCMI_RST_A53_7	S32G3_SCMI_RST(4)
+#define S32G3_SCMI_RST_MAX_ID	S32G3_SCMI_RST(5)
+
+#if S32CC_SCMI_RST_MAX_ID < S32G3_SCMI_RST_MAX_ID
+#error Please increase the value of S32CC_SCMI_RST_MAX_ID
+#endif
+
+#endif
+
