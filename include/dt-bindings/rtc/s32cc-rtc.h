ea03f63856ffb2a6465a1d6698783026ac87dd72
Radu Pirea <radu-nicolae.pirea@nxp.com>
2022-09-30 16:23:31 +0300

    dt-bindings: rtc: add S32CC rtc bindings
    Issue: ALB-9114
    
    Signed-off-by: Radu Pirea <radu-nicolae.pirea@nxp.com>

---
+++ include/dt-bindings/rtc/s32cc-rtc.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: (GPL-2.0+ OR BSD-3-Clause) */
+/*
+ * Copyright 2022 NXP
+ */
+
+#ifndef _DT_BINDINGS_S32CC_RTC_H
+#define _DT_BINDINGS_S32CC_RTC_H
+
+/* Clock sources - usable with RTCC_CLKSEL */
+#define S32CC_RTC_SOURCE_SIRC	0x0
+#define S32CC_RTC_SOURCE_FIRC	0x2
+
+#endif
