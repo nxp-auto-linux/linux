19802ced418472a595ced16caaba3dbd8a64138d
Ghennadi Procopciuc <ghennadi.procopciuc@nxp.com>
2023-01-12 10:27:02 +0200

    dt-bindings/mux: s32cc: Add IDs for CAN timestamping
    Issue: ALB-9261
    Signed-off-by: Ghennadi Procopciuc <ghennadi.procopciuc@nxp.com>

---
+++ include/dt-bindings/mux/nxp-s32g-can-ts.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright 2023 NXP
+ *
+ * This header provides constants for CAN timesptamp sources for NXP S32G SoCs
+ */
+#ifndef _DT_BINDINGS_MUX_NXP_S32G_CAN_TS
+#define _DT_BINDINGS_MUX_NXP_S32G_CAN_TS
+
+#define S32G_CAN_TS_DISABLE		0x0
+#define S32G_CAN_TS_GMAC		0x4
+#define S32G_CAN_TS_STM7		0x5
+#define S32G_CAN_TS_PFE_MAC		0x6
+
+#endif
