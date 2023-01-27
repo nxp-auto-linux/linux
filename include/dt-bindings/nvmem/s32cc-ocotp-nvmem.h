affac1cb9fe8f5a091e6bacb864f351e6a7a3613
Ciprian Costea <ciprianmarian.costea@nxp.com>
2023-01-27 11:18:40 +0200

    dt-bindings: Add TMU Fuse NVMEM cell offset and size
    Issue: ALB-9677
    Signed-off-by: Ciprian Costea <ciprianmarian.costea@nxp.com>

---
+++ include/dt-bindings/nvmem/s32cc-ocotp-nvmem.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright 2023 NXP
+ */
+
+/* NRAM offset and size for TMU Fuse */
+#define TMU_FUSE_OFFSET		(0x298)
+
+#define OCOTP_CELL_SIZE		(0x4)
