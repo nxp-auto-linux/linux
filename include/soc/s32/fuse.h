23b4e910f199022f7e11f188ccf5a3798e264f33
Ciprian Costea <ciprianmarian.costea@nxp.com>
2023-02-15 11:08:12 +0200

    s32: ocotp: Add ocotp driver
    It adds support for reading fuses from the shadow registers.
    
    Issue: ALB-9677
    Signed-off-by: Ciprian Costea <ciprianmarian.costea@nxp.com>

---
+++ include/soc/s32/fuse.h
@@ -0,0 +1,33 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright 2023 NXP
+ *
+ */
+
+#ifndef __SOC_S32_FUSE_H__
+#define __SOC_S32_FUSE_H__
+
+#include <soc/s32/fuse_defs.h>
+#include <soc/s32/nvmem_common.h>
+
+static inline
+int s32_ocotp_nvmem_get_tmu_fuse(struct device *dev,
+				 const char *cname,
+				 union s32_tmu_fuse *tmu_fuse)
+{
+	char *buf = NULL;
+
+	if (!tmu_fuse)
+		return -EINVAL;
+
+	buf = read_nvmem_cell(dev, cname);
+	if (IS_ERR(buf))
+		return PTR_ERR(buf);
+
+	tmu_fuse->R = *(s32 *)buf;
+
+	kfree(buf);
+	return 0;
+}
+
+#endif /* __SOC_S32_FUSE_H__*/
