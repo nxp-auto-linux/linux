834b67d8071240f6d738500289b186dec5a52a94
Martin Hrdlicka <martin.hrdlicka@nxp.com>
2023-03-01 10:46:37 +0100

    arm64: dts: s32g-pfe: extend enum for nxp,pfeng-linked-phyif property
    Extend nxp,pfeng-linked-phyif ethernet property enum to support
    also HIF channels.
    
    Issue: AAVB-6542
    Signed-off-by: Martin Hrdlicka <martin.hrdlicka@nxp.com>

--- include/dt-bindings/net/s32g-pfe.h
+++ include/dt-bindings/net/s32g-pfe.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 /*
- * Copyright 2021-2022 NXP
+ * Copyright 2021-2023 NXP
  *
  */
 
@@ -13,8 +13,13 @@
 #define PFE_HIF_CHANNEL_3		3
 #define PFE_HIF_CHANNEL_NOCPY		16
 
-#define PFE_EMAC_0			0
-#define PFE_EMAC_1			1
-#define PFE_EMAC_2			2
+#define PFE_PHYIF_EMAC_0		0
+#define PFE_PHYIF_EMAC_1		1
+#define PFE_PHYIF_EMAC_2		2
+#define PFE_PHYIF_HIF_NOCPY		4
+#define PFE_PHYIF_HIF_0			6
+#define PFE_PHYIF_HIF_1			7
+#define PFE_PHYIF_HIF_2			8
+#define PFE_PHYIF_HIF_3			9
 
 #endif /* __DT_BINDINGS_NET_S32G_PFE_H__ */
