652197e7e98f767c0bdb5e3224a545af99fe8d3e
Ghennadi Procopciuc <ghennadi.procopciuc@nxp.com>
2023-04-25 09:57:03 +0300

    dt-bindings: mailbox: Add channel for LLCE CAN Core
    A new configuration channel type is needed for LLCE CAN Core
    driver.
    
    Issue: ALB-9781
    Signed-off-by: Ghennadi Procopciuc <ghennadi.procopciuc@nxp.com>

--- include/dt-bindings/mailbox/nxp-llce-mb.h
+++ include/dt-bindings/mailbox/nxp-llce-mb.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 /*
- * Copyright 2020-2022 NXP
+ * Copyright 2020-2023 NXP
  */
 #ifndef _DT_BINDINGS_MAILBOX_NXP_LLCE_MB_H_
 #define _DT_BINDINGS_MAILBOX_NXP_LLCE_MB_H_
@@ -14,5 +14,6 @@
 #define S32G_LLCE_CAN_TX_MB		0x3
 #define S32G_LLCE_CAN_LOGGER_MB		0x4
 #define S32G_LLCE_CAN_LOGGER_CONFIG_MB	0x5
+#define S32G_LLCE_CAN_CORE_CONFIG_MB	0x6
 
 #endif
