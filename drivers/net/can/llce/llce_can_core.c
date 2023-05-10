02f19d8b1567d897dad17394ecd4d2cc65b74a75
Ghennadi Procopciuc <ghennadi.procopciuc@nxp.com>
2023-05-10 13:05:34 +0300

    net/can: llce_can_core: Create llce_can_core.h
    Extract some of the structures used by llce_can_core into a dedicated
    header file to make them available to the debugfs interface, which will
    be added in later commits.
    
    Issue: ALB-9781
    Signed-off-by: Ghennadi Procopciuc <ghennadi.procopciuc@nxp.com>

--- drivers/net/can/llce/llce_can_core.c
+++ drivers/net/can/llce/llce_can_core.c
@@ -1,44 +1,17 @@
 // SPDX-License-Identifier: GPL-2.0+ OR BSD-3-Clause
 /* Copyright 2023 NXP */
 #include <linux/can/dev.h>
-#include <linux/can/dev/llce_can_common.h>
-#include <linux/mailbox/nxp-llce/llce_mailbox.h>
-#include <linux/mailbox_client.h>
 #include <linux/mailbox_controller.h>
 #include <linux/module.h>
 #include <linux/moduleparam.h>
 #include <linux/netdevice.h>
 #include <linux/platform_device.h>
 
+#include "llce_can_core.h"
+
 #define LLCE_CAN_MAX_FILTER_MB		(16U)
 
 #define INVALID_HW_FIFO			(0xFFU)
-
-struct filter_state {
-	bool enabled;
-	bool advanced;
-	union {
-		struct llce_can_rx_filter base;
-		struct llce_can_advanced_filter advanced;
-	} f;
-	struct list_head link;
-};
-
-struct can_ctrl_state {
-	struct filter_state *base_filter;
-	struct filter_state *logging_filter;
-	bool logging;
-	u8 fifo;
-};
-
-struct llce_can_core {
-	struct can_ctrl_state ctrls[LLCE_CAN_CONFIG_MAXCTRL_COUNT];
-	struct notifier_block notifier;
-	struct completion config_cmd_done;
-	struct mbox_client config_client;
-	struct list_head filters_list;
-	struct mbox_chan *config;
-};
 
 static int get_filter_id(struct filter_state *filter, u16 *id)
 {
@@ -86,13 +59,6 @@ static struct can_ctrl_state *get_ctrl_state(struct llce_can_core *can_core,
 		return NULL;
 
 	return &can_core->ctrls[hw_ctrl];
-}
-
-static struct device *get_can_core_dev(struct llce_can_core *can_core)
-{
-	struct mbox_chan *conf_chan = can_core->config;
-
-	return llce_can_chan_dev(conf_chan);
 }
 
 static int llce_get_fifo(struct llce_can_core *can_core, u8 hw_ctrl, u8 *fifo)
---
+++ drivers/net/can/llce/llce_can_core.h
@@ -0,0 +1,46 @@
+/* SPDX-License-Identifier: GPL-2.0+ OR BSD-3-Clause */
+/* Copyright 2023 NXP */
+
+#ifndef LLCE_CAN_CORE_H
+#define LLCE_CAN_CORE_H
+
+#include <linux/can/dev/llce_can_common.h>
+#include <linux/mailbox/nxp-llce/llce_mailbox.h>
+#include <linux/mailbox_client.h>
+#include <linux/notifier.h>
+#include <linux/types.h>
+
+struct filter_state {
+	bool enabled;
+	bool advanced;
+	union {
+		struct llce_can_rx_filter base;
+		struct llce_can_advanced_filter advanced;
+	} f;
+	struct list_head link;
+};
+
+struct can_ctrl_state {
+	struct filter_state *base_filter;
+	struct filter_state *logging_filter;
+	bool logging;
+	u8 fifo;
+};
+
+struct llce_can_core {
+	struct can_ctrl_state ctrls[LLCE_CAN_CONFIG_MAXCTRL_COUNT];
+	struct notifier_block notifier;
+	struct completion config_cmd_done;
+	struct mbox_client config_client;
+	struct list_head filters_list;
+	struct mbox_chan *config;
+};
+
+static inline struct device *get_can_core_dev(struct llce_can_core *can_core)
+{
+	struct mbox_chan *conf_chan = can_core->config;
+
+	return llce_can_chan_dev(conf_chan);
+}
+
+#endif
