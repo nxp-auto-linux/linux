f1e278d4bd4a0837064d974d7e2c2b4afb5121e6
Ghennadi Procopciuc <ghennadi.procopciuc@nxp.com>
2023-05-17 09:45:04 +0300

    mailbox: llce: Change the encoding of the filter id
    This is a workaround to overcome the limitation introduced in LLCE FW
    1.0.6 where the value of Llce_Can_ReceiveFilterType.u16FilterId must
    not exceed LLCE_CONFIG_RXLUT_ENTRY_COUNT, although this field was
    designed to contain user-defined data.
    
    Issue: ALB-10049
    Issue: ALB-9449
    Signed-off-by: Ghennadi Procopciuc <ghennadi.procopciuc@nxp.com>

--- include/linux/mailbox/nxp-llce/llce_mailbox.h
+++ include/linux/mailbox/nxp-llce/llce_mailbox.h
@@ -7,6 +7,15 @@
 #include <linux/mailbox/nxp-llce/llce_can.h>
 #include <uapi/linux/can.h>
 
+/* LLCE CAN filter structure */
+#define LLCE_FILTER_HW_CTRL_SHIFT	0
+#define LLCE_FILTER_HW_CTRL_MASK	((u16)GENMASK(7, LLCE_FILTER_HW_CTRL_SHIFT))
+
+#define LLCE_FILTER_MB_TYPE_SHIFT	8
+#define LLCE_FILTER_MB_TYPE_MASK	((u16)GENMASK(15, LLCE_FILTER_MB_TYPE_SHIFT))
+
+#define LLCE_HW_CTRL_FROM_CHAN_IDX	(0xffu)
+
 struct llce_mb;
 
 enum llce_chan_state {
@@ -83,4 +92,32 @@ struct logger_config_msg {
 	enum logger_config_cmd cmd;
 	bool fw_logger_support;
 };
+
+static inline void llce_filter_set_mb_type(u16 *filter, bool long_mb)
+{
+	*filter &= ~LLCE_FILTER_MB_TYPE_MASK;
+
+	if (long_mb)
+		*filter |= (USE_LONG_MB << LLCE_FILTER_MB_TYPE_SHIFT);
+	else
+		*filter |= (USE_SHORT_MB << LLCE_FILTER_MB_TYPE_SHIFT);
+}
+
+static inline void llce_filter_set_hwctrl(u16 *filter, u8 hwctrl)
+{
+	*filter &= LLCE_FILTER_HW_CTRL_MASK;
+
+	*filter |= (hwctrl << LLCE_FILTER_HW_CTRL_SHIFT);
+}
+
+static inline u16 llce_filter_get_mb_type(u16 filter)
+{
+	return (filter & LLCE_FILTER_MB_TYPE_MASK) >> LLCE_FILTER_MB_TYPE_SHIFT;
+}
+
+static inline u8 llce_filter_get_hw_ctrl(u16 filter)
+{
+	return (filter & LLCE_FILTER_HW_CTRL_MASK) >> LLCE_FILTER_HW_CTRL_SHIFT;
+}
+
 #endif
