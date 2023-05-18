430705b29a946424916c481f2d137600f5a5a7d6
Ghennadi Procopciuc <ghennadi.procopciuc@nxp.com>
2023-05-18 12:31:22 +0300

    net/can: llce_can_core: Add filters control via debugfs
    This introduces LLCE CAN filters management through debugfs. This
    support includes list, add, remove and change status commands.
    
    Issue: ALB-9781
    Signed-off-by: Ghennadi Procopciuc <ghennadi.procopciuc@nxp.com>

--- drivers/net/can/llce/llce_can_core_debugfs.h
+++ drivers/net/can/llce/llce_can_core_debugfs.h
@@ -19,6 +19,17 @@ struct debugfs_cmd_block {
 	debugfs_cmd_get_val_t get_val;
 };
 
+struct debugfs_choice {
+	const char *name;
+	u64 id;
+};
+
+struct debugfs_choice_block {
+	const struct debugfs_choice *choices;
+	const struct debugfs_choice *selected;
+	size_t n_choices;
+};
+
 struct llce_can_dest_debugfs {
 	u8 dest_hw_ch_list[LLCE_CAN_CONFIG_MAXCTRL_COUNT];
 	u32 routing_opts;
@@ -28,8 +39,27 @@ struct llce_can_dest_debugfs {
 	u8 id;
 };
 
+struct llce_can_filters_debugfs {
+	struct llce_can_rx_filter base_opts;
+	struct llce_can_advanced_feature adv_opts;
+	struct debugfs_choice_block status;
+	/* Basic options */
+	struct debugfs_choice_block entry;
+	struct debugfs_choice_block mb_type;
+	/* Advanced options */
+	struct debugfs_choice_block auth;
+	struct debugfs_choice_block host;
+	struct debugfs_choice_block logging;
+	struct debugfs_choice_block custom_proc;
+	struct debugfs_cmd_block add_exec;
+	struct debugfs_cmd_block set_status_exec;
+	struct debugfs_cmd_block remove_exec;
+	u8 hw_ctrl;
+};
+
 struct llce_can_core_debugfs {
 	struct llce_can_dest_debugfs can_dest;
+	struct llce_can_filters_debugfs can_filters;
 	struct dentry *root_dir;
 };
 
