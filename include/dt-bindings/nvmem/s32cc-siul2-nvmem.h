cc6cb742eb905430e2511a9299d2d3a11cdcd89d
Andrei Cherechesu <andrei.cherechesu@nxp.com>
2023-05-30 16:26:35 +0300

    dt-bindings: nvmem: s32cc: Sync SIUL2 NVMEM cells with SCMI NVMEM ones
    Made 3 changes to the S32CC SIUL2 NVMEM cells in order to sync them to
    the ones provided by the S32CC SCMI NVMEM driver:
    	- split the SOC_REVISION cell into SOC_MAJOR and SOC_MINOR
    	- renamed the PCIE_VARIANT_OFFSET to PCIE_DEV_ID_OFFSET to match
    the new cell name
    	- reordered offsets per SIUL module to be consecutive
    
    Issue: ALB-9554
    Signed-off-by: Andrei Cherechesu <andrei.cherechesu@nxp.com>

--- include/dt-bindings/nvmem/s32cc-siul2-nvmem.h
+++ include/dt-bindings/nvmem/s32cc-siul2-nvmem.h
@@ -1,13 +1,14 @@
 /* SPDX-License-Identifier: GPL-2.0+ */
 /*
- * Copyright 2021-2022 NXP
+ * Copyright 2021-2023 NXP
  */
 
 /* NVRAM offset and size for SIUL2_0 module */
-#define SOC_REVISION_OFFSET		(0x0)
-#define PCIE_VARIANT_OFFSET		(0x4)
+#define SOC_MAJOR_OFFSET		(0x0)
+#define SOC_MINOR_OFFSET		(0x1)
+#define PCIE_DEV_ID_OFFSET		(0x2)
 
 /* NVRAM offset and size for SIUL2_1 module */
-#define SERDES_PRESENCE_OFFSET	(0x0)
+#define SERDES_PRESENCE_OFFSET		(0x0)
 
 #define NVRAM_CELL_SIZE			(0x4)
