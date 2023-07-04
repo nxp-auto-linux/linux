117410bd47d8d6163578ba36195a5a2b85a329cf
Ghennadi Procopciuc <ghennadi.procopciuc@nxp.com>
2023-07-04 22:53:12 +0300

    dt-bindings/reset: Update S32G reset header
    Issue: ALB-9660
    Signed-off-by: Ghennadi Procopciuc <ghennadi.procopciuc@nxp.com>

--- include/dt-bindings/reset/s32g-scmi-reset.h
+++ include/dt-bindings/reset/s32g-scmi-reset.h
@@ -1,14 +1,14 @@
 /* SPDX-License-Identifier: BSD-3-Clause */
 /*
- * Copyright 2021 NXP
+ * Copyright 2021, 2023 NXP
  */
 #ifndef __DT_BINDINGS_S32G_SCMI_RESET_H
 #define __DT_BINDINGS_S32G_SCMI_RESET_H
 
 #include <dt-bindings/reset/s32cc-scmi-reset.h>
 
-#define S32G_SCMI_RST_PFE		S32CC_PLAT_SCMI_RST(0)
-#define S32G_SCMI_RST_LLCE		S32CC_PLAT_SCMI_RST(1)
+#define S32G_SCMI_RST_PFE	S32CC_PLAT_SCMI_RST(0)
+#define S32G_SCMI_RST_LLCE	S32CC_PLAT_SCMI_RST(1)
 #define S32G_SCMI_RST_MAX_ID	S32CC_PLAT_SCMI_RST(2)
 
 #if S32CC_SCMI_RST_MAX_ID < S32G_SCMI_RST_MAX_ID
