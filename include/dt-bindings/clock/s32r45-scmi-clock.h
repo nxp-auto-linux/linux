0f9b05e74c3dff9e671f5beaca732e2cb7c51541
Ghennadi Procopciuc <ghennadi.procopciuc@nxp.com>
2023-07-05 08:22:54 +0300

    dt-bindings/clock: Update S32R45 clock header
    Issue: ALB-9660
    Signed-off-by: Ghennadi Procopciuc <ghennadi.procopciuc@nxp.com>

--- include/dt-bindings/clock/s32r45-scmi-clock.h
+++ include/dt-bindings/clock/s32r45-scmi-clock.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: BSD-3-Clause */
 /*
- * Copyright 2020-2021 NXP
+ * Copyright 2021-2023 NXP
  */
 #ifndef __DT_BINDINGS_SCMI_CLOCK_S32R45_H
 #define __DT_BINDINGS_SCMI_CLOCK_S32R45_H
