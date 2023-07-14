2832a3a713f3ccf98e926c73fcd7a2973d7030c4
Ghennadi Procopciuc <ghennadi.procopciuc@nxp.com>
2023-07-14 13:16:42 +0300

    dt-bindings/reset: Rename S32GEN1 to S32CC IDs
    Issue: ALB-9660
    Signed-off-by: Ghennadi Procopciuc <ghennadi.procopciuc@nxp.com>

--- include/dt-bindings/reset/s32g-scmi-reset.h
+++ include/dt-bindings/reset/s32g-scmi-reset.h
@@ -5,14 +5,14 @@
 #ifndef __DT_BINDINGS_S32G_SCMI_RESET_H
 #define __DT_BINDINGS_S32G_SCMI_RESET_H
 
-#include <dt-bindings/reset/s32gen1-scmi-reset.h>
+#include <dt-bindings/reset/s32cc-scmi-reset.h>
 
-#define S32G_SCMI_RST_PFE	S32GEN1_PLAT_SCMI_RST(0)
-#define S32G_SCMI_RST_LLCE	S32GEN1_PLAT_SCMI_RST(1)
-#define S32G_SCMI_RST_MAX_ID	S32GEN1_PLAT_SCMI_RST(2)
+#define S32G_SCMI_RST_PFE	S32CC_PLAT_SCMI_RST(0)
+#define S32G_SCMI_RST_LLCE	S32CC_PLAT_SCMI_RST(1)
+#define S32G_SCMI_RST_MAX_ID	S32CC_PLAT_SCMI_RST(2)
 
-#if S32GEN1_SCMI_RST_MAX_ID < S32G_SCMI_RST_MAX_ID
-#error Please increase the value of S32GEN1_SCMI_RST_MAX_ID
+#if S32CC_SCMI_RST_MAX_ID < S32G_SCMI_RST_MAX_ID
+#error Please increase the value of S32CC_SCMI_RST_MAX_ID
 #endif
 
 #endif
--- include/dt-bindings/reset/s32r45-scmi-reset.h
+++ include/dt-bindings/reset/s32r45-scmi-reset.h
@@ -6,14 +6,14 @@
 #ifndef __DT_BINDINGS_SCMI_RESET_S32R45_H
 #define __DT_BINDINGS_SCMI_RESET_S32R45_H
 
-#include <dt-bindings/reset/s32gen1-scmi-reset.h>
+#include <dt-bindings/reset/s32cc-scmi-reset.h>
 
-#define S32R45_SCMI_RST_LAX	S32GEN1_PLAT_SCMI_RST(0)
-#define S32R45_SCMI_RST_RADAR	S32GEN1_PLAT_SCMI_RST(1)
-#define S32R45_SCMI_RST_MAX_ID	S32GEN1_PLAT_SCMI_RST(2)
+#define S32R45_SCMI_RST_LAX	S32CC_PLAT_SCMI_RST(0)
+#define S32R45_SCMI_RST_RADAR	S32CC_PLAT_SCMI_RST(1)
+#define S32R45_SCMI_RST_MAX_ID	S32CC_PLAT_SCMI_RST(2)
 
-#if S32GEN1_SCMI_RST_MAX_ID < S32R45_SCMI_RST_MAX_ID
-#error Please increase the value of S32GEN1_SCMI_RST_MAX_ID
+#if S32CC_SCMI_RST_MAX_ID < S32R45_SCMI_RST_MAX_ID
+#error Please increase the value of S32CC_SCMI_RST_MAX_ID
 #endif
 
 #endif
