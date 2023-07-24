1abb7fc0a7492caedba0b6bc94b8bbeb4d867700
Andrei Stefanescu <andrei.stefanescu@nxp.com>
2023-07-24 16:26:46 +0300

    dt-bindings: s32cc: add more memory ranges
    Add more memory ranges to the s32cc SIUL2 bindings. This is because the
    SIUL2 register range has a lot of small gaps.
    
    Issue: ALB-10214
    
    Signed-off-by: Andrei Stefanescu <andrei.stefanescu@nxp.com>

--- include/dt-bindings/memory/s32cc-siul2.h
+++ include/dt-bindings/memory/s32cc-siul2.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0-or-later OR BSD-3-Clause */
 /*
- * Copyright 2022 NXP
+ * Copyright 2022-2023 NXP
  */
 
 #ifndef __DT_BINDINGS_S32CC_SIUL2_MEMORY_H__
@@ -13,13 +13,41 @@
 #define MSCRS_SIUL2_1_0		(4)
 #define MSCRS_SIUL2_1_1		(5)
 #define IMCRS_SIUL2_0		(6)
-#define IMCRS_SIUL2_1_0		(7)
-#define IMCRS_SIUL2_1_EIRQS	(8)
-#define IMCRS_SIUL2_1_1		(9)
-#define OPADS_SIUL2_0		(10)
-#define OPADS_SIUL2_1		(11)
-#define IPADS_SIUL2_0		(12)
-#define IPADS_SIUL2_1		(13)
-#define EIRQS_SIUL2_1		(14)
+#define IMCRS_SIUL2_0_0		(6)
+#define IMCRS_SIUL2_0_1		(7)
+#define IMCRS_SIUL2_0_2		(8)
+#define IMCRS_SIUL2_1_0		(9)
+#define IMCRS_SIUL2_1_1		(10)
+#define IMCRS_SIUL2_1_2		(11)
+#define IMCRS_SIUL2_1_3		(12)
+#define IMCRS_SIUL2_1_4		(13)
+#define IMCRS_SIUL2_1_5		(14)
+#define IMCRS_SIUL2_1_6		(15)
+#define IMCRS_SIUL2_1_7		(16)
+#define IMCRS_SIUL2_1_8		(17)
+#define IMCRS_SIUL2_1_9		(18)
+#define IMCRS_SIUL2_1_10        (19)
+#define IMCRS_SIUL2_1_11	(20)
+#define IMCRS_SIUL2_1_12	(21)
+#define IMCRS_SIUL2_1_13	(22)
+#define IMCRS_SIUL2_1_14	(23)
+#define IMCRS_SIUL2_1_15	(24)
+#define IMCRS_SIUL2_1_16	(25)
+#define IMCRS_SIUL2_1_17	(26)
+#define IMCRS_SIUL2_1_18	(27)
+#define IMCRS_SIUL2_1_19	(28)
+#define IMCRS_SIUL2_1_EIRQS	(29)
+#define IMCRS_SIUL2_1_21	(30)
+#define IMCRS_SIUL2_1_22	(31)
+#define IMCRS_SIUL2_1_23	(32)
+#define IMCRS_SIUL2_1_24	(33)
+#define IMCRS_SIUL2_1_25	(34)
+#define IMCRS_SIUL2_1_26	(35)
+#define IMCRS_SIUL2_1_27	(36)
+#define OPADS_SIUL2_0		(37)
+#define OPADS_SIUL2_1		(38)
+#define IPADS_SIUL2_0		(39)
+#define IPADS_SIUL2_1		(40)
+#define EIRQS_SIUL2_1		(41)
 
 #endif /*  __DT_BINDINGS_S32CC_SIUL2_MEMORY_H__ */
