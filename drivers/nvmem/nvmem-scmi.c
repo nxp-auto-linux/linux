9c4d258a6905f6eecfc5dd54cc758f718780b20d
Andrei Cherechesu <andrei.cherechesu@nxp.com>
2023-08-11 09:43:58 +0300

    drivers/nvmem: nvmem-scmi: Add WRITE_CELL command support
    Added the WRITE_CELL command support to the nvmem-scmi provider
    driver, which uses the NVMEM SCMI Firmware driver to the relay messages
    to a SCMI platform.
    
    Issue: ALB-10366
    Signed-off-by: Andrei Cherechesu <andrei.cherechesu@nxp.com>

--- drivers/nvmem/nvmem-scmi.c
+++ drivers/nvmem/nvmem-scmi.c
@@ -40,13 +40,34 @@ static int scmi_nvmem_read_cell(void *priv, unsigned int offset, void *val,
 	return 0;
 }
 
+static int scmi_nvmem_write_cell(void *priv, unsigned int offset, void *val,
+				 size_t bytes)
+{
+	struct nvmem_scmi_data *dev_priv = priv;
+	struct device *dev = dev_priv->dev;
+	int ret;
+	u32 value = 0;
+
+	memcpy(&value, val, bytes);
+
+	ret = nvmem_ops->nvmem_write_cell(dev_priv->ph, offset, bytes, value);
+	if (ret) {
+		dev_err(dev, "Failed to write cell %u with size %lu, value: %u\n",
+			offset, bytes, value);
+		return ret;
+	}
+
+	return 0;
+}
+
 static struct nvmem_config scmi_nvmem_config = {
 	.name = "nvmem_scmi",
 	.owner = THIS_MODULE,
 	.word_size = 1,
 	.size = 4,
-	.read_only = true,
+	.read_only = false,
 	.reg_read = scmi_nvmem_read_cell,
+	.reg_write = scmi_nvmem_write_cell,
 };
 
 static int scmi_nvmem_probe(struct scmi_device *sdev)
