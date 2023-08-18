42161a43b71e5b6d85ffa69814572a0170bf788d
Andrei Cherechesu <andrei.cherechesu@nxp.com>
2023-08-18 15:47:14 +0300

    dt-bindings: nvmem: Add s32cc-gpr-nvmem bindings
    Added bindings for the s32cc-gpr-nvmem driver which will be
    used as an NVMEM provider for reading/writing GPR cells from
    Linux.
    
    Issue: ALB-10366
    Signed-off-by: Andrei Cherechesu <andrei.cherechesu@nxp.com>

---
+++ include/dt-bindings/nvmem/s32cc-gpr-nvmem.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright 2023 NXP
+ */
+
+#ifndef S32CC_GPR_NVMEM_H
+#define S32CC_GPR_NVMEM_H
+
+#define S32CC_GPR_DDR_PMU_IRQ_OFFSET	(0x0)
+
+#define S32CC_GPR_CELL_SIZE		(0x4)
+
+#endif
