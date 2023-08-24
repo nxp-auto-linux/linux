465cadf14ae56c254004ac14b5fa918ea718391a
Andrei Cherechesu <andrei.cherechesu@nxp.com>
2023-08-24 17:15:10 +0300

    dt-bindings: s32r45-gpr-nvmem: Add GMAC1_PHY_INTF_SEL cell offset
    Issue: ALB-10366
    Signed-off-by: Andrei Cherechesu <andrei.cherechesu@nxp.com>

---
+++ include/dt-bindings/nvmem/s32r45-gpr-nvmem.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright 2023 NXP
+ */
+
+#ifndef S32R45_GPR_NVMEM_H
+#define S32R45_GPR_NVMEM_H
+
+#define S32R45_GPR_GMAC1_PHY_INTF_SEL_OFFSET    (0x100)
+
+#define S32R45_GPR_CELL_SIZE                    (0x4)
+
+#endif
