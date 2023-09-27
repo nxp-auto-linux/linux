a33b780ed22885853531180fea570d0f8b35968a
Andrei Cherechesu <andrei.cherechesu@nxp.com>
2023-09-27 13:45:15 +0300

    dt-bindings: s32g-gpr-nvmem: Add PFE-related cells
    Added offsets for PFE_EMACS_INTF_SEL, PFE_COH_EN, PFE_PWR_CTRL,
    PFE_EMACS_GENCTRL1 and PFE_GENCTRL3 NVMEM cells, which are all
    related and used by the PFE driver as a consumer.
    
    Issue: ALB-10366
    Signed-off-by: Andrei Cherechesu <andrei.cherechesu@nxp.com>

---
+++ include/dt-bindings/nvmem/s32g-gpr-nvmem.h
@@ -0,0 +1,16 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright 2023 NXP
+ */
+#ifndef S32G_GPR_NVMEM_H
+#define S32G_GPR_NVMEM_H
+
+#define S32G_GPR_PFE_EMACS_INTF_SEL_OFFSET      (0x80)
+#define S32G_GPR_PFE_COH_EN_OFFSET              (0x81)
+#define S32G_GPR_PFE_PWR_CTRL_OFFSET            (0x82)
+#define S32G_GPR_PFE_EMACS_GENCTRL1_OFFSET      (0x83)
+#define S32G_GPR_PFE_GENCTRL3_OFFSET            (0x84)
+
+#define S32G_GPR_CELL_SIZE                      (0x4)
+
+#endif
